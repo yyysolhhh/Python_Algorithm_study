36d7322d4daed03296197408924675f6ead40ef9
yyysolhhh <solbaram37@gmail.com>
2021-08-01 15:58:11 +0900

    210801

--- 백준 2869.py
+++ 백준 2869.py
@@ -1,13 +1,17 @@
 import sys
+import math
 A, B, V = map(int, sys.stdin.readline().split())
-day = V//(A-B) - 1
-h = (A-B) * day
-V -= h
-while (h <= V):
-    h += A
-    day += 1
-    if (h >= V):
-        break
-    else:
-        h -= B
+# day = V//(A-B) - 1
+# h = (A-B) * day
+# V -= h
+# while (h <= V):
+#     h += A
+#     day += 1
+#     if (h >= V):
+#         break
+#     else:
+#         h -= B
+# print(day)
+
+day = math.ceil((V - A) / (A - B)) + 1
 print(day)
