32d22fbc306f6b2f2088bcb532e03fda0db98c3e
yyysolhhh <solbaram37@gmail.com>
2021-08-15 18:22:01 +0900

    210815

--- 백준 2447.py
+++ 백준 2447.py
@@ -1,49 +1,72 @@
-# N = int(input())
 
-
-# def stars(x):
-#     for _ in range(x):
-#         for i in range(1, 4):
-#             for j in range(1, 4):
-#                 if (i == 2 and j == 2):
-#                     print(" ", end="")
-#                     continue
-#                 print("*", end="")
-#             print()
-
-
-# stars(N)
-
-# 별 찍는 재귀 함수
-def draw_star(n):
+def stars(x):
     global Map
+    if x == 3:
+        for _ in range(x):
+            for i in range(3):
+                for j in range(3):
+                    if (i == 1 and j == 1):
+                        continue
+                    else:
+                        Map[i][j] = 1
+        return Map
 
-    if n == 3:
-        Map[0][:3] = Map[2][:3] = [1]*3
-        Map[1][:3] = [1, 0, 1]
-        return
-
-    a = n//3
-    draw_star(n//3)
+    a = x // 3
+    stars(a)
     for i in range(3):
         for j in range(3):
             if i == 1 and j == 1:
                 continue
             for k in range(a):
-                Map[a*i+k][a*j:a*(j+1)] = Map[k][:a]  # 핵심 아이디어
+                Map[a*i+k][a*j:a*(j+1)] = Map[k][:a]
 
 
 N = int(input())
 
-# 메인 데이터 선언
-Map = [[0 for i in range(N)] for i in range(N)]
-
-draw_star(N)
+Map = [[0 for _ in range(N)] for _ in range(N)]
+stars(N)
 
 for i in Map:
     for j in i:
-        if j:
-            print('*', end='')
+        if j == 1:
+            print("*", end="")
         else:
-            print(' ', end='')
+            print(" ", end="")
     print()
+
+# 다른 사람 코드
+# stars(N)
+
+# # 별 찍는 재귀 함수
+# def draw_star(n):
+#     global Map
+
+#     if n == 3:
+#         Map[0][:3] = Map[2][:3] = [1]*3
+#         Map[1][:3] = [1, 0, 1]
+#         return
+
+#     a = n//3
+#     draw_star(n//3)
+#     for i in range(3):
+#         for j in range(3):
+#             if i == 1 and j == 1:
+#                 continue
+#             for k in range(a):
+#                 Map[a*i+k][a*j:a*(j+1)] = Map[k][:a]  # 핵심 아이디어
+
+
+# N = int(input())
+
+# # 메인 데이터 선언
+# Map = [[0 for i in range(N)] for i in range(N)]
+
+# draw_star(N)
+
+# for i in Map:
+#     for j in i:
+#         if j:
+#             print('*', end='')
+#         else:
+#             print(' ', end='')
+#     print()
