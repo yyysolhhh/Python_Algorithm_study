c90a328c776dc8d295c2ad76341181d9a32ae091
yyysolhhh <solbaram37@gmail.com>
2021-08-18 23:15:03 +0900

    210818

---
+++ 백준 5622.py
@@ -0,0 +1,20 @@
+Word = input()
+t = 0
+for i in Word:
+    if i == 'A' or i == 'B' or i == 'C':
+        t += 3
+    elif i == 'D' or i == 'E' or i == 'F':
+        t += 4
+    elif i == 'G' or i == 'H' or i == 'I':
+        t += 5
+    elif i == 'J' or i == 'K' or i == 'L':
+        t += 6
+    elif i == 'M' or i == 'N' or i == 'O':
+        t += 7
+    elif i == 'P' or i == 'Q' or i == 'R' or i == 'S':
+        t += 8
+    elif i == 'T' or i == 'U' or i == 'V':
+        t += 9
+    elif i == 'W' or i == 'X' or i == 'Y' or i == 'Z':
+        t += 10
+print(t)
