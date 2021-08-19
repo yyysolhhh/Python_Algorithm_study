fdacd95281f9936bb7c8937f79f256fae6ca5fd3
yyysolhhh <solbaram37@gmail.com>
2021-08-19 16:27:46 +0900

    210819

--- 백준 10828.py
+++ 백준 10828.py
@@ -1,7 +1,7 @@
+# 조건문 이용
 import sys
 N = int(sys.stdin.readline())
 stack = []
-print(len(stack))
 for _ in range(N):
     word = sys.stdin.readline().split()
     order = word[0]
@@ -25,3 +25,53 @@ for _ in range(N):
             print(-1)
         else:
             print(stack[-1])
+
+# 함수 이용
+# import sys
+
+
+# def push(x):
+#     stack.append(x)
+
+
+# def pop():
+#     if len(stack) == 0:
+#         print(-1)
+#     else:
+#         top = stack.pop()
+#         print(top)
+
+
+# def size():
+#     print(len(stack))
+
+
+# def empty():
+#     if len(stack) == 0:
+#         print(1)
+#     else:
+#         print(0)
+
+
+# def top():
+#     if len(stack) == 0:
+#         print(-1)
+#     else:
+#         print(stack[-1])
+
+
+# N = int(sys.stdin.readline())
+# stack = []
+# for _ in range(N):
+#     word = sys.stdin.readline().split()
+#     order = word[0]
+#     if order == "push":
+#         push(word[1])
+#     elif order == "pop":
+#         pop()
+#     elif order == "size":
+#         size()
+#     elif order == "empty":
+#         empty()
+#     elif order == "top":
+#         top()
