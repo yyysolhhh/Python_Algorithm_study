f8ffc5f171eb593502a69728e3981f67b2eb0a51
yyysolhhh <solbaram37@gmail.com>
2021-08-19 22:37:53 +0900

    210819

--- 백준 9012.py
+++ 백준 9012.py
@@ -1,14 +1,44 @@
 T = int(input())
 for _ in range(T):
     string = input()
-    l = 0
-    r = 0
+    sum = 0
     for i in string:
         if i == "(":
-            l += 1
+            sum += 1
         elif i == ")":
-            r += 1
-    if l == r:
+            sum -= 1
+        if sum < 0:
+            print("NO")
+            break
+    if sum == 0:
         print("YES")
-    else:
+    elif sum > 0:
         print("NO")
+
+# 2 stack 이용
+T = int(input())
+
+
+def vps(string):
+    stack = []
+    for i in string:
+        if i == "(":
+            stack.append(i)
+        elif i == ")":
+            if not stack:
+                print("NO")
+                return
+            else:
+                stack.pop()
+
+    if not stack:
+        print("YES")
+        return
+    elif stack:
+        print("NO")
+        return
+
+
+for _ in range(T):
+    string = input()
+    vps(string)
