fbe2572703859a90022ada2df7d8a77e10c3b231
yyysolhhh <solbaram37@gmail.com>
2021-08-21 12:54:28 +0900

    210821

--- 백준 1874.py
+++ 백준 1874.py
@@ -4,3 +4,10 @@ stack = []
 for _ in range(n):
     num.append(int(input()))
 for i in range(1, n+1):
+    for j in num:
+        if i != j:
+            stack.append(i)
+            print("+")
+        elif i == j:
+            stack.pop()
+            print("-")
