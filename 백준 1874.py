d60c9096fe0cbfa8f1c5b2ff686fd2bb1fe54cdc
yyysolhhh <solbaram37@gmail.com>
2021-08-22 23:42:38 +0900

    210822

--- 백준 1874.py
+++ 백준 1874.py
@@ -1,13 +1,17 @@
 n = int(input())
-num = []
 stack = []
-for _ in range(n):
-    num.append(int(input()))
-for i in range(1, n+1):
-    for j in num:
-        if i != j:
-            stack.append(i)
-            print("+")
-        elif i == j:
-            stack.pop()
-            print("-")
+operation = []
+for i in range(n):
+    num = int(input())
+    for j in range(1, num+1):
+        operation.append("+")
+        stack.append(j)
+
+# for i in range(1, n+1):
+#     for j in num:
+#         if i != j:
+#             stack.append(i)
+#             print("+")
+#         elif i == j:
+#             stack.pop()
+#             print("-")
