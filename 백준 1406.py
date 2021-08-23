2d73244ea39d604605e97ec7cb90cc738c6098ba
yyysolhhh <solbaram37@gmail.com>
2021-08-23 18:58:54 +0900

    210823

--- 백준 1406.py
+++ 백준 1406.py
@@ -1,15 +1,21 @@
 import sys
-N = list(sys.stdin.readline())
+string = list(sys.stdin.readline())
+N = len(string)
 M = int(sys.stdin.readline())
 cursor = N
 for _ in range(M):
     command = sys.stdin.readline()
     if command[0] == "L":
-        cursor -= 1
-
+        if cursor != 0:
+            cursor -= 1
     elif command[0] == "D":
-        cursor += 1
-
+        if cursor != N:
+            cursor += 1
     elif command[0] == "B":
-
+        if cursor != 0:
+            del string[cursor-1]
+            cursor -= 1
     elif command[0] == "P":
+        string.insert(cursor-1, command[2])
+        cursor += 1
+print("".join(string))
