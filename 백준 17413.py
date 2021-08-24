3d98d00202c21dde32897f80dd1dd6beff3c2e70
yyysolhhh <solbaram37@gmail.com>
2021-08-25 03:51:03 +0900

    210825

--- 백준 17413.py
+++ 백준 17413.py
@@ -6,12 +6,20 @@ while i <= len(S):
     if S[i] == "<":
         while S[i] != ">":
             i += 1
+            print(1)
+        i += 1
     elif S[i].isalnum():
         start = i
         while S[i] != "<":
             i += 1
-        word = list(S[start:i].split())
-        S[start:i] = word.reverse()
+            print(2)
+        # word = list(reversed(S[start:i]))
+        # S[start:i] = word
+        # S[start:i].reverse()
+        S[start:i] = list(S[i:start:-1])
+        print(S)
+    elif S[i] == " ":
+        i += 1
 print(S)
 # print(tag+string[::-1], end='')
 # print(" ", end='')
