f110f2cc19f36c10775e0fb159292a07368d26cd
yyysolhhh <solbaram37@gmail.com>
2021-08-25 22:21:17 +0900

    210825

--- 백준 17413.py
+++ 백준 17413.py
@@ -1,25 +1,58 @@
+# 14~23줄
+# import sys
+# S = sys.stdin.readline()
+# i = 0
+# while i <= len(S):
+#     if S[i] == "<":
+#         while S[i] != ">":
+#             i += 1
+#         i += 1
+#     elif S[i].isalnum():
+#         start = i
+#         while S[i] != "<":
+#             i += 1
+#         # word = S[start:i]
+#         word = S[i-1:start-1:-1]
+#         print(word)
+#         # word.reverse()
+#         # print(word)
+#         # S[start:i] = S[i-1:start-1:-1]
+#         S[start:i] = word
+#         # word = "".join(reversed(S[start:i]))
+#         # S[start:i].reverse()
+#         # S[start:i] = list(S[i:start:-1])
+#         print(S)
+#     elif S[i] == " ":
+#         i += 1
+# print(S)
+# # print(tag+string[::-1], end='')
+# # print(" ", end='')
+
 import sys
 S = sys.stdin.readline()
-i = 0
-word = []
-while i <= len(S):
+tag = False
+ans = []
+mid = []
+reverse = []
+index = []
+for i in range(len(S)):
     if S[i] == "<":
-        while S[i] != ">":
-            i += 1
-            print(1)
-        i += 1
-    elif S[i].isalnum():
-        start = i
-        while S[i] != "<":
-            i += 1
-            print(2)
-        # word = list(reversed(S[start:i]))
-        # S[start:i] = word
-        # S[start:i].reverse()
-        S[start:i] = list(S[i:start:-1])
-        print(S)
-    elif S[i] == " ":
-        i += 1
-print(S)
-# print(tag+string[::-1], end='')
-# print(" ", end='')
+        tag = True
+        ans.append(S[i])
+        continue
+    elif S[i] == ">":
+        tag = False
+        ans.append(S[i])
+        index.append(i)
+        continue
+    if tag == True:
+        ans.append(S[i])
+    else:
+        mid.append(S[i])
+    mid = "".join(mid).strip()
+    for i in mid.split():
+        reverse.append(i[::-1])
+        reverse.append(" ")
+
+ans.insert(index[int((len(index)/2-1))]+1, "".join(reverse).strip())
+print("".join(ans))
