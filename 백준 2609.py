bb953867f166052e6f729cbb6c357af7db2bf99b
yyysolhhh <solbaram37@gmail.com>
2021-08-25 23:50:03 +0900

    210825 최대공약수와 최소공배수

--- 백준 2609.py
+++ 백준 2609.py
@@ -1,19 +1,35 @@
-n, m = map(int, input().split())
-nd, md = [], []
-cd = 0
-# n 약수
-for i in range(1, n+1):
-    if n % i == 0:
-        nd.append(i)
-# m 약수
-for i in range(1, m+1):
-    if m % i == 0:
-        md.append(i)
-# n, m 최대공약수
-for i in nd:
-    for j in md:
-        if i == j:
-            cd = j
-# 최소공배수
-cm = int((n/cd) * (m/cd) * cd)
-print(cd, cm)
+# n, m = map(int, input().split())
+# nd, md = [], []
+# gcd = 0
+# # n 약수
+# for i in range(1, n+1):
+#     if n % i == 0:
+#         nd.append(i)
+# # m 약수
+# for i in range(1, m+1):
+#     if m % i == 0:
+#         md.append(i)
+# # n, m 최대공약수
+# for i in nd:
+#     for j in md:
+#         if i == j:
+#             gcd = j
+# # 최소공배수
+# lcm = int((n/cd) * (m/cd) * cd)
+# print(gcd, lcm)
+
+a, b = map(int, input().split())
+
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+print(gcd(a, b))
+print(lcm(a, b))
