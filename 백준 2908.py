e7624a3f743b1fd81b41d7544198ff0b047edd1f
yyysolhhh <solbaram37@gmail.com>
2021-08-26 16:19:01 +0900

    210826

--- 백준 2908.py
+++ 백준 2908.py
@@ -1,3 +1,4 @@
+# 1
 A, B = input().split()
 for i in range(2, -1, -1):
     if int(A[i]) < int(B[i]):
@@ -8,3 +9,12 @@ for i in range(2, -1, -1):
         break
     else:
         continue
+
+# 2
+A, B = input().split()
+A = int(A[::-1])
+B = int(B[::-1])
+if A > B:
+    print(A)
+else:
+    print(B)
