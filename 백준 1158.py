c78e38ea31858f270020d5427cf119ce13b421b2
yyysolhhh <solbaram37@gmail.com>
2021-08-28 16:31:16 +0900

    210828

--- 백준 1158.py
+++ 백준 1158.py
@@ -2,15 +2,14 @@ N, K = input().split()
 N = int(N)
 K = int(K)
 circle = [i for i in range(1, N+1)]
-o = K - 1
+josephus = []
+order = K - 1
 while circle:
-    print(o)
-    if o > N:
-        o = N - K - 1
+    if order > N:
+        order = N - K - 1
     if N == 1:
-        o = 0
-    print(N, o)
-    del circle[o]
+        order = 0
+    josephus.append(str(circle.pop(order)))
     N -= 1
-    o += K - 1
-    print(circle)
+    order += K - 1
+print("<" + ", ".join(josephus) + ">")
