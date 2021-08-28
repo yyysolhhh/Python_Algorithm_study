1c2122c28dd341ad54eadfb6588aeeb682915aaa
yyysolhhh <solbaram37@gmail.com>
2021-08-29 05:15:28 +0900

    210829

--- 백준 6588.py
+++ 백준 6588.py
@@ -1,3 +1,4 @@
+# 내 풀이
 import sys
 
 
@@ -5,6 +6,8 @@ def goldbach(n):
     for i in range(3, n, 2):
         if isPrime(i) and isPrime(n-i):
             return i, n-i
+        else:
+            print("Goldbach's conjecture is wrong.")
 
 
 def isPrime(n):
@@ -21,3 +24,30 @@ while(1):
     if n == 0:
         break
     print("%d = %d + %d" % (n, goldbach(n)[0], goldbach(n)[1]))
+
+# 2 다른 사람 풀이
+
+
+def Goldbach():
+    check = [False, False] + [True] * 1000000
+    for i in range(2, 1001):
+        if check[i] == True:
+            for j in range(i + i, 1000001, i):
+                check[j] = False
+    while True:
+        n = int(sys.stdin.readline())
+        if n == 0:
+            break
+        A = 0
+        B = n
+        for _ in range(1000000):
+            if check[A] and check[B]:
+                print(f"{n} = {A} + {B}")
+                break
+            A += 1
+            B -= 1
+        else:
+            print("Goldbach's conjecture is worng.")
+
+
+Goldbach()
