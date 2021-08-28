2f21581bbff55365a6883758f6dc7dc71c51d33e
yyysolhhh <solbaram37@gmail.com>
2021-08-28 22:49:33 +0900

    210828 시간 초과

--- 백준 1929.py
+++ 백준 1929.py
@@ -1,8 +1,9 @@
-M, N = map(int, input().split())
+import sys
+M, N = map(int, sys.stdin.readline().split())
 for i in range(M, N+1):
-    count_d = 0
+    count_d = 0   # 약수 개수
     for j in range(1, i+1):
         if i % j == 0:
-            count_d += 1
+            count_d += 1    # M, N 사이의 수 i를 1, i 사이의 수 j로 나눈 나머지가 0일 때 약수 개수 +1
     if count_d == 2:
-        print(i)
+        print(i)    # 약수 개수가 2일 때 소수 출력
