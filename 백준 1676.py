db1844347ac1feb183c4c1ba64d2b257e1b73ae8
yyysolhhh <solbaram37@gmail.com>
2021-08-29 21:37:58 +0900

    210829

--- 백준 1676.py
+++ 백준 1676.py
@@ -1,14 +1,14 @@
 import sys
-# # 내 풀이: 일단 팩토리얼 구하고 뒤집어서 0개수 구함
-# N = int(input())
-# factorial = 1
-# for i in range(1, N+1):
-#     factorial *= i
-# fac_str = list(reversed(str(factorial)))
-# for j in range(len(fac_str)):
-#     if int(fac_str[j]) != 0:
-#         print(j)
-#         break
+# 내 풀이: 일단 팩토리얼 구하고 뒤집어서 0개수 구함
+N = int(input())
+factorial = 1
+for i in range(1, N+1):
+    factorial *= i
+fac_str = list(reversed(str(factorial)))
+for j in range(len(fac_str)):
+    if int(fac_str[j]) != 0:
+        print(j)
+        break
 
 # 다른 사람 풀이: 5가 곱해진 개수가 0의 개수
 input = sys.stdin.readline
