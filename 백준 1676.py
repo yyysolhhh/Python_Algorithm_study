8b323d93b389ee22ddd46972f13673f143662927
yyysolhhh <solbaram37@gmail.com>
2021-08-29 13:59:50 +0900

    210829

--- 백준 1676.py
+++ 백준 1676.py
@@ -2,8 +2,8 @@ N = int(input())
 factorial = 1
 for i in range(1, N+1):
     factorial *= i
-fac_str = str(factorial)
-for j in range(-1, -len(fac_str), -1):
+fac_str = list(reversed(str(factorial)))
+for j in range(len(fac_str)):
     if int(fac_str[j]) != 0:
-        print(-j-1)
+        print(j)
         break
