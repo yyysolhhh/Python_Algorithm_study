b05ab2ec459eb991e1eceb365e90207857d16cd0
yyysolhhh <solbaram37@gmail.com>
2021-08-31 12:51:53 +0900

    210831 문자열 분석 EOFError 처리

---
+++ 백준 10820.py
@@ -0,0 +1,16 @@
+while(True):
+    try:
+        string = input()
+        num = [0 for _ in range(4)]
+        for i in string:
+            if i.islower():
+                num[0] += 1
+            elif i.isupper():
+                num[1] += 1
+            elif i.isdecimal():
+                num[2] += 1
+            elif i == " ":
+                num[3] += 1
+        print(" ".join(map(str, num)))
+    except EOFError:
+        break
