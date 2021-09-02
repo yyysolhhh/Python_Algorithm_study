a96fefc5f04ebd82ecd43d76ebcf490c46670178
yyysolhhh <solbaram37@gmail.com>
2021-09-02 23:28:03 +0900

    210902

--- 백준 9613.py
+++ 백준 9613.py
@@ -12,7 +12,7 @@ for _ in range(t):
     n = list(map(int, sys.stdin.readline().split()))
     combi = [[] for _ in range(len(n)**2)]
     gcd_list = []
-    # for i in range(1, len(n)-1):  n[1]부터 조합 만드려다가 실패
+    # for i in range(1, len(n)-1):  # n[1]부터 조합 만드려다가 실패
     #     for j in range(len(n)):
     #         combi.append([n[j], n[j+i]])
     # print(combi)
@@ -20,3 +20,6 @@ for _ in range(t):
     #   gcd_list.append(gcd(combi[i]))
     # gcd_list=set(gcd_list)
     # print(gcd_list)
+    for i in range(1, len(n)-1):
+        gcd_list.append(gcd(n[i], n[i+1]))
+    print(set(gcd_list))
