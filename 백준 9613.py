b5a3341a46144a43a0b82f1bfb2a269ef6d6430e
yyysolhhh <solbaram37@gmail.com>
2021-09-04 00:11:47 +0900

    210904 gcd 합

--- 백준 9613.py
+++ 백준 9613.py
@@ -16,9 +16,9 @@ def gcd(a, b):
 
 t = int(sys.stdin.readline())
 for _ in range(t):
-    n = list(map(int, sys.stdin.readline().split()))
-    combi = [[] for _ in range(len(n)**2)]
-    gcd_list = []
+    num = list(map(int, sys.stdin.readline().split()))
+    # combi = [[] for _ in range(len(n)**2)]
+    # gcd_list = []
     # for i in range(1, len(n)-1):  # n[1]부터 조합 만드려다가 실패
     #     for j in range(len(n)):
     #         combi.append([n[j], n[j+i]])
@@ -27,6 +27,11 @@ for _ in range(t):
     #   gcd_list.append(gcd(combi[i]))
     # gcd_list=set(gcd_list)
     # print(gcd_list)
-    for i in range(1, len(n)-1):
-        gcd_list.append(gcd(n[i], n[i+1]))
-    print(set(gcd_list))
+    # 문제 잘못 이해함. 중복 제거해야 되는걸로 이해함.
+    sum = 0
+    n = num.pop(0)
+    for i in range(n):
+        for j in range(n):
+            if i < j:
+                sum += gcd(num[i], num[j])
+    print(sum)
