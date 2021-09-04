8bc53a747da87bba27e76b9baa05fa01061c3145
yyysolhhh <solbaram37@gmail.com>
2021-09-05 01:57:28 +0900

    210905 이건 안되겠다

--- 백준 2745.py
+++ 백준 2745.py
@@ -1,13 +1,24 @@
 N, B = input().split()
 
 
+# def baseConversion(B, N):
+#     decimal = 0
+#     N = list(N)
+#     for i in range(len(N), 0, -1):
+#         if N[-i].isalpha():
+#             N[-i] = ord(N[-i]) - 55
+#         decimal += int(N[-i]) * B ** (i - 1)
+#     return decimal
+
 def baseConversion(B, N):
     decimal = 0
     N = list(N)
-    for i in range(len(N), 0, -1):
-        if N[-i].isalpha():
-            N[-i] = ord(N[-i]) - 55
-        decimal += int(N[-i]) * B ** (i - 1)
+    for i in N[::-1]:
+        index = N.index(i)
+        if i.isalpha():
+            i = ord(i) - 55
+        decimal += int(i) * B ** (len(N)-index)
+        print(len(N)-index, i, decimal)
     return decimal
 
 
