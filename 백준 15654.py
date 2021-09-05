981a75d1a024f73d514a54cd34cf808a4cbcc1f5
yyysolhhh <solbaram37@gmail.com>
2021-09-05 18:36:41 +0900

    210905 틀림

---
+++ 백준 15654.py
@@ -0,0 +1,17 @@
+def series():
+    if len(combi) == M:
+        print(" ".join(map(str, combi)))
+        return
+    for i in num:
+        if i in combi:
+            continue
+        combi.append(i)
+        series()
+        combi.pop()
+
+
+N, M = map(int, input().split())
+num = input().split()
+num.sort()
+combi = []
+series()
