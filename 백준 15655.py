d381343134ea5fecd27d074622d2f696b46cf8f3
yyysolhhh <solbaram37@gmail.com>
2021-09-05 18:46:17 +0900

    210905 N과M

--- 백준 15655.py
+++ 백준 15655.py
@@ -1,2 +1,15 @@
+def series(start):
+    if len(combi) == M:
+        print(' '.join(map(str, combi)))
+        return
+    for i in range(start, len(num)):
+        combi.append(num[i])
+        series(i+1)
+        combi.pop()
+
+
 N, M = map(int, input().split())
-num = map(int, input().split())
+num = list(map(int, input().split()))
+num.sort()
+combi = []
+series(0)
