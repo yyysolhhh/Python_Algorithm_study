7fa1ea993dae251f04a8c7ec5d68d7a622f3bd02
yyysolhhh <solbaram37@gmail.com>
2021-09-09 03:17:14 +0900

    210909 또 시간초과

--- 백준 17103.py
+++ 백준 17103.py
@@ -2,24 +2,26 @@ import sys
 input = sys.stdin.readline
 
 
-def primeNum(n):
-    divi = 0
-    for i in range(1, n+1):
-        if n % i == 0:
-            divi += 1
-    if divi == 2:
-        return True
-    else:
-        return False
+def checkPrimeNum(n):
+    primeNumList = []
+    for i in range(2, n+1):
+        divi = 0
+        for j in range(1, i+1):
+            if i % j == 0:
+                divi += 1
+        if divi == 2:
+            primeNumList.append(i)
+    return primeNumList
 
 
 T = int(input())
-for _ in range(T):
-    N = int(input())
+N = [int(input()) for _ in range(T)]
+primeNum = checkPrimeNum(max(N))
+for i in N:
     goldbachPartition = 0
-    for i in range(1, N//2+1):
-        if primeNum(i):
-            if primeNum(N-i):
+    for j in range(1, i//2+1):
+        if j in primeNum:
+            if i-j in primeNum:
                 goldbachPartition += 1
-                print(i, N-i, end='|')
+                # print(j, i-j, end='|')
     print(goldbachPartition)
