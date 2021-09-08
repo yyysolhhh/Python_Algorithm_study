850e54e2e44b6d19d8bbea1273c91090a5d2cc55
yyysolhhh <solbaram37@gmail.com>
2021-09-09 01:59:13 +0900

    210909

--- 백준 3085.py
+++ 백준 3085.py
@@ -1,3 +1,14 @@
+def checkContinuousPart():
+    for
+
+
 N = int(input())
 color = list(list(input()) for _ in range(N))
-print(color)
+maxNum = 0
+for i in range(N):
+    for j in range(N):
+        # 옆자리 바꾸기
+        color[i][j], color[i][j+1] = color[i][j+1], color[i][j]
+        checkContinuousPart()
+        # 원위치
+        color[i][j], color[i][j+1] = color[i][j+1], color[i][j]
