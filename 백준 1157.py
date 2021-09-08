f773fa3a9b06da00aa299489a11ace0c496ec479
yyysolhhh <solbaram37@gmail.com>
2021-09-09 03:48:38 +0900

    210909 단어 공부

--- 백준 1157.py
+++ 백준 1157.py
@@ -1,8 +1,21 @@
-import string
+# import string
+# word = input()
+# alphabet = string.ascii_letters
+# big = string.ascii_uppercase
+# num = []
+# for i in alphabet:
+#     num.append(word.count(i))
+# print(big[num.index(max(num))])
+
 word = input()
-alphabet = string.ascii_letters
-big = string.ascii_uppercase
-num = []
-for i in alphabet:
-    num.append(word.count(i))
-print(big[num.index(max(num))])
+alphabetNum = [0 for _ in range(26)]
+for i in word:
+    if i.islower():
+        alphabetNum[ord(i)-97] += 1
+    elif i.isupper():
+        alphabetNum[ord(i)-65] += 1
+if alphabetNum.count(max(alphabetNum)) > 1:
+    best = '?'
+else:
+    best = chr(alphabetNum.index(max(alphabetNum))+65)
+print(best)
