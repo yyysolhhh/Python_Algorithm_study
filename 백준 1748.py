8eabfb22ea338e4c4ed97425492f507af48ef581
yyysolhhh <solbaram37@gmail.com>
2021-09-11 02:53:41 +0900

    210911 수 이어 쓰기

--- 백준 1748.py
+++ 백준 1748.py
@@ -1,17 +1,27 @@
 import sys
 
+# 시간 초과
+# def digit(n):
+#     return len(str(n))
 
-def digit(n):
-    num = list(str(n))
-    return len(num)
 
+# N = int(sys.stdin.readline())
+# result = 0
+# if N < 10:
+#     result = N
+# elif N < 100 and N >= 10:
+#     result += 9 + (N - 9) * 2
+# else:
+#     result += 9 + 90 * 2
+#     for i in range(100, N+1):
+#         result += digit(i)
+# print(result)
 
+# 2
 N = int(sys.stdin.readline())
 result = 0
-if N < 10:
-    result = N
-else:
-    result += 9
-    for i in range(10, N+1):
-        result += digit(i)
+len_N = len(str(N))
+for i in range(1, len_N):
+    result += i * 9 * 10 ** (i - 1)
+result += (N - 10 ** (len_N-1) + 1) * (len_N)
 print(result)
