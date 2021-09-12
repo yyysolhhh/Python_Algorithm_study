8a5d980ad58322b649c59790de047c942c6dc70f
yyysolhhh <solbaram37@gmail.com>
2021-09-13 00:22:46 +0900

    210913 크로아티아 알파벳

--- 백준 2941.py
+++ 백준 2941.py
@@ -1,4 +1,16 @@
 changed = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-word = input()
-for i in changed:
-    for j in wo
+word = list(input())
+result = 0
+while word:
+    if ''.join(word[0:2]) in changed:
+        result += 1
+        for _ in range(2):
+            word.pop(0)
+    elif ''.join(word[0:3]) in changed:
+        result += 1
+        for _ in range(3):
+            word.pop(0)
+    else:
+        result += 1
+        word.pop(0)
+print(result)
