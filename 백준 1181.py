a759b508268ce7df1c458a0f3634a76d71500c95
yyysolhhh <solbaram37@gmail.com>
2021-09-13 12:04:37 +0900

    210913 단어 정렬

--- 백준 1181.py
+++ 백준 1181.py
@@ -1,10 +1,11 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
-word = list(input() for _ in range(N))
+word = list(input().strip() for _ in range(N))
 word = sorted(set(word))
 l = []
 for i in word:
     l.append(len(i))
 while l:
-    print(word[l.index(min(l))])
-    word.pop(l.index(min(l)))
+    print(word.pop(l.index(min(l))))
     l.remove(min(l))
