4c7d7e6463688bba69df7139bbbdd90fd770eaed
yyysolhhh <solbaram37@gmail.com>
2021-09-14 00:36:43 +0900

    210914 시간초과

---
+++ 백준 1920.py
@@ -0,0 +1,9 @@
+N = int(input())
+A = list(map(int, input().split()))
+M = int(input())
+nums = list(map(int, input().split()))
+for i in nums:
+    if i in A:
+        print(1)
+    else:
+        print(0)
