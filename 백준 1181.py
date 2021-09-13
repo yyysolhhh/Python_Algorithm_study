d638f15819b2797092c1dec0b8bea77bf6d0c5e0
yyysolhhh <solbaram37@gmail.com>
2021-09-14 00:29:34 +0900

    210914 단어 정렬 풀이 추가

--- 백준 1181.py
+++ 백준 1181.py
@@ -4,12 +4,12 @@ N = int(input())
 word = list(input().strip() for _ in range(N))
 word = sorted(set(word))
 # 내 풀이
-# l = []
-# for i in word:
-#     l.append(len(i))
-# while l:
-#     print(word.pop(l.index(min(l))))
-#     l.remove(min(l))
+l = []
+for i in word:
+    l.append(len(i))
+while l:
+    print(word.pop(l.index(min(l))))
+    l.remove(min(l))
 
 # 다른 사람 풀이
 word.sort(key=len)
