e6c6819fa88c0f1f4b3fc5a2b950e9374f3c5b05
yyysolhhh <solbaram37@gmail.com>
2021-09-13 12:07:44 +0900

    210913 단어 정렬

--- 백준 1181.py
+++ 백준 1181.py
@@ -3,9 +3,15 @@ input = sys.stdin.readline
 N = int(input())
 word = list(input().strip() for _ in range(N))
 word = sorted(set(word))
-l = []
+# 내 풀이
+# l = []
+# for i in word:
+#     l.append(len(i))
+# while l:
+#     print(word.pop(l.index(min(l))))
+#     l.remove(min(l))
+
+# 다른 사람 풀이
+word.sort(key=len)
 for i in word:
-    l.append(len(i))
-while l:
-    print(word.pop(l.index(min(l))))
-    l.remove(min(l))
+    print(i)
