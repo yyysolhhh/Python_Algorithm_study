83d0ef8da2cede9f1ce9591c86f87af9678eeedb
yyysolhhh <solbaram37@gmail.com>
2021-09-14 22:58:57 +0900

    210914 숨바꼭질

--- 백준 17087.py
+++ 백준 17087.py
@@ -9,12 +9,21 @@ def gcd(x, y):
         x, y = y, x % y
     return x
 
+# 시간 초과
+# N, S = map(int, input().split())
+# A = list(map(int, input().split()))
+# D = list(set(abs(i-S) for i in A))
+# ans = []
+# for i in range(len(D)-1):
+#     for j in range(i+1, len(D)):
+#         ans.append(gcd(D[i], D[j]))
+# print(min(ans))
+
 
 N, S = map(int, input().split())
 A = list(map(int, input().split()))
 D = list(set(abs(i-S) for i in A))
-ans = []
-for i in range(len(D)-1):
-    for j in range(i+1, len(D)):
-        ans.append(gcd(D[i], D[j]))
-print(min(ans))
+ans = D[0]
+for i in range(1, len(D)):
+    ans = gcd(ans, D[i])
+print(ans)
