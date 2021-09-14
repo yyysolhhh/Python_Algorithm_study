42c5af71c0932850a95ff272431c6155a742eacc
yyysolhhh <solbaram37@gmail.com>
2021-09-14 22:14:49 +0900

    210914 퇴사 dp

--- 백준 14501.py
+++ 백준 14501.py
@@ -2,17 +2,26 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 T, P = [], []
-case1 = 0
-case2 = 0
+dp = [0 for i in range(N+1)]
 for _ in range(N):
     t, p = map(int, input().split())
     T.append(t)
     P.append(p)
 for i in range(N-1, -1, -1):
-    if T[i] > N - i:
-        continue
+    if T[i] + i > N:
+        dp[i] = dp[i+1]
     else:
-        current = P[i] + max(case1, case2)
-        case2 = case1
-        case1 = current
-print(max(case1, case2))
+        dp[i] = max(dp[i+1], P[i] + dp[i+T[i]])
+print(dp[0])
+
+# 실패
+# case1 = 0
+# case2 = 0
+# for i in range(N-1, -1, -1):
+#     if T[i] > N - i:
+#         continue
+#     else:
+#         current = max(case1, P[i] + )
+#         case2 = case1
+#         case1 = current
+# print(max(case1, case2))
