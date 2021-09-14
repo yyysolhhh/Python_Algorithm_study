12c233d03b6c1cedce9152621ebcbf10a224c263
yyysolhhh <solbaram37@gmail.com>
2021-09-14 22:47:28 +0900

    210914 시간초과

--- 백준 17087.py
+++ 백준 17087.py
@@ -1,4 +1,9 @@
+import sys
+input = sys.stdin.readline
+
 # 동생들과의 거리의 최대공약수 찾기
+
+
 def gcd(x, y):
     while y:
         x, y = y, x % y
@@ -7,14 +12,9 @@ def gcd(x, y):
 
 N, S = map(int, input().split())
 A = list(map(int, input().split()))
-D = []
+D = list(set(abs(i-S) for i in A))
 ans = []
-for i in A:
-    D.append(abs(i - S))
-print(D)
 for i in range(len(D)-1):
     for j in range(i+1, len(D)):
-        print(D[i], D[j])
         ans.append(gcd(D[i], D[j]))
-        print(ans)
 print(min(ans))
