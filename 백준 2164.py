6861001c2c317a3a025fee824183a1f4c57c2816
yyysolhhh <solbaram37@gmail.com>
2021-09-16 00:45:03 +0900

    210916 시간초과

--- 백준 2164.py
+++ 백준 2164.py
@@ -1,5 +1,7 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
-queue = [i for i in range(1, N+1)]
+queue = [i for i in range(2, N+1, 2)]
 while len(queue) != 1:
     queue.pop(0)
     queue.append(queue.pop(0))
