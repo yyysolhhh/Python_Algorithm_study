1eabdb51f8397cf192936fef45e751f165ed3e41
yyysolhhh <solbaram37@gmail.com>
2021-09-16 01:12:41 +0900

    210916 deque 문제

--- 백준 2164.py
+++ 백준 2164.py
@@ -1,8 +1,17 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 N = int(input())
-queue = [i for i in range(2, N+1, 2)]
-while len(queue) != 1:
-    queue.pop(0)
-    queue.append(queue.pop(0))
-print(queue[0])
+# 시간 초과
+# queue = [i for i in range(2, N+1, 2)]
+# while len(queue) != 1:
+#     queue.pop(0)
+#     queue.append(queue.pop(0))
+# print(queue[0])
+
+# 2
+deq = deque([i for i in range(1, N+1)])
+while len(deq) != 1:
+    deq.popleft()
+    deq.append(deq.popleft())
+print(deq[0])
