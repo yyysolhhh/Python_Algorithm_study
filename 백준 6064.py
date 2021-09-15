5876544677cdd89fdd42fc084b86018619a0fcdb
yyysolhhh <solbaram37@gmail.com>
2021-09-15 23:18:41 +0900

    210915 시간초과

--- 백준 6064.py
+++ 백준 6064.py
@@ -5,16 +5,13 @@ for _ in range(T):
     x1 = min(M, N)
     y1 = min(M, N)
     while True:
-        try:
-            x1 += 1
-            y1 += 1
-            year += 1
-            if x1 > M:
-                x1 = 1
-            if y1 > N:
-                y1 = 1
-            if x1 == x and y1 == y:
-                print(year)
-                break
-        except:
-            print(-1)
+        x1 += 1
+        y1 += 1
+        year += 1
+        if x1 > M:
+            x1 = 1
+        if y1 > N:
+            y1 = 1
+        if x1 == x and y1 == y:
+            print(year)
+            break
