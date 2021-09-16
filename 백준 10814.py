ab817d05dd595c3d9049bef32431c6253afd3d3b
yyysolhhh <solbaram37@gmail.com>
2021-09-17 03:11:57 +0900

    210917 나이순 정렬

--- 백준 10814.py
+++ 백준 10814.py
@@ -1,7 +1,15 @@
 N = int(input())
 member = []
+# 틀림
+# for i in range(N):
+#     member.append([i, input().split()])
+# member.sort(key=lambda x: (x[1][0], x[0]))
+# for j in member:
+#     print(j[1][0], j[1][1])
+
+# 2
 for i in range(N):
-    member.append([i, input().split()])
-member.sort(key=lambda x: (-int(x[1][0]), x[0]))
-for j in member:
-    print(j[1][0], j[1][1])
+    member.append(list(input().split()))
+member.sort(key=lambda x: int(x[0]))
+for i in member:
+    print(i[0], i[1])
