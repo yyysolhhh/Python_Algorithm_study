e3cd84d4f4d619a6e7c9b8e3afcc78a0c03ce910
yyysolhhh <solbaram37@gmail.com>
2021-09-17 17:45:10 +0900

    210917 쇠막대기

--- 백준 10799.py
+++ 백준 10799.py
@@ -1,21 +1,63 @@
+# 시간 초과
+# import sys
+# bra = sys.stdin.readline().strip()
+# stick = []
+# open = []
+# for i in range(len(bra)):
+#     if bra[i:i+2] == '((':
+#         stick.append(1)
+#         open.append(True)
+#         # print('((', stick, open)
+#     elif bra[i:i+2] == '()':
+#         for j in range(len(stick)):
+#             if open[j]:
+#                 stick[j] += 1
+#         # print('()', stick, open)
+#     elif bra[i:i+2] == '))':
+#         for j in range(len(open)-1, -1, -1):
+#             if open[j]:
+#                 open[j] = False
+#                 break
+#         # print('))', stick, open)
+# print(sum(stick))
+
+# 시간 초과
+# import sys
+# bra = sys.stdin.readline().strip()
+# stick = []
+# result = 0
+# for i in range(len(bra)):
+#     if bra[i:i+2] == '((':
+#         stick.append(1)
+#     elif bra[i:i+2] == '()':
+#         stick += [1]*len(stick)
+#     elif bra[i:i+2] == '))':
+#         result += stick.pop()
+# print(result)
+
 import sys
 bra = sys.stdin.readline().strip()
 stick = []
-open = []
+result = 0
 for i in range(len(bra)):
-    if bra[i:i+2] == '((':
+    # 1
+    # if bra[i] == '(':
+    #     stick.append(1)
+    #     result += 1
+    # else:
+    #     if bra[i-1] == '(':
+    #         stick.pop()
+    #         result += len(stick) - 1
+    #     else:
+    #         stick.pop()
+    # 2
+    if bra[i] == '(':
         stick.append(1)
-        open.append(True)
-        # print('((', stick, open)
-    elif bra[i:i+2] == '()':
-        for j in range(len(stick)):
-            if open[j]:
-                stick[j] += 1
-        # print('()', stick, open)
-    elif bra[i:i+2] == '))':
-        for j in range(len(open)-1, -1, -1):
-            if open[j]:
-                open[j] = False
-                break
-        # print('))', stick, open)
-print(sum(stick))
+    else:
+        if bra[i-1] == '(':
+            stick.pop()
+            result += len(stick)
+        else:
+            stick.pop()
+            result += 1
+print(result)
