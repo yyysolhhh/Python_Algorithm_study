e2e0b73deb35b40d180081c3d4082b419c958803
yyysolhhh <solbaram37@gmail.com>
2021-09-17 17:23:10 +0900

    210917 시간초과

--- 백준 10799.py
+++ 백준 10799.py
@@ -1,8 +1,21 @@
 import sys
-bra = sys.stdin.readline()
+bra = sys.stdin.readline().strip()
 stick = []
+open = []
 for i in range(len(bra)):
-    if bra[i:i+2] == "((":
-        while(bra[i:i+2] != "))"):
-            stick.append(i)
-            if bra[i:i+2]
+    if bra[i:i+2] == '((':
+        stick.append(1)
+        open.append(True)
+        # print('((', stick, open)
+    elif bra[i:i+2] == '()':
+        for j in range(len(stick)):
+            if open[j]:
+                stick[j] += 1
+        # print('()', stick, open)
+    elif bra[i:i+2] == '))':
+        for j in range(len(open)-1, -1, -1):
+            if open[j]:
+                open[j] = False
+                break
+        # print('))', stick, open)
+print(sum(stick))
