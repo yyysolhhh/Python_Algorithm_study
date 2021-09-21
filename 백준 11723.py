21d450bb5c1f14e704ecda2c0d3c774043519dae
yyysolhhh <solbaram37@gmail.com>
2021-09-21 16:43:41 +0900

    210921 집합

--- 백준 11723.py
+++ 백준 11723.py
@@ -4,15 +4,9 @@ input = sys.stdin.readline
 
 def operate(S, op, num):
     if op == 'add':
-        if num not in S:
-            S.append(num)
-        else:
-            return
+        S.add(num)
     elif op == 'remove':
-        if num in S:
-            S.remove(num)
-        else:
-            return
+        S.discard(num)
     elif op == 'check':
         if num in S:
             print(1)
@@ -20,19 +14,19 @@ def operate(S, op, num):
             print(0)
     elif op == 'toggle':
         if num in S:
-            S.remove(num)
+            S.discard(num)
         else:
-            S.append(num)
+            S.add(num)
 
 
 M = int(input())
-S = []
+S = set()
 for _ in range(M):
     operation = input().split()
-    if operation[0] == 'all':
-        S = [str(i) for i in range(1, 21)]
-    elif operation[0] == 'empty':
-        S = []
+    if len(operation) == 1:
+        if operation[0] == 'all':
+            S = {str(i) for i in range(1, 21)}
+        elif operation[0] == 'empty':
+            S = set()
     else:
         operate(S, operation[0], operation[1])
-    print(S)
