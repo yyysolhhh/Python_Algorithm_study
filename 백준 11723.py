ba03fb58eeabb678ac6d5275ea6a1e5b54f6e557
yyysolhhh <solbaram37@gmail.com>
2021-09-21 15:46:26 +0900

    210921 시간초과

--- 백준 11723.py
+++ 백준 11723.py
@@ -1,12 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+
 def operate(S, op, num):
     if op == 'add':
         if num not in S:
             S.append(num)
-
+        else:
+            return
     elif op == 'remove':
         if num in S:
             S.remove(num)
-
+        else:
+            return
     elif op == 'check':
         if num in S:
             print(1)
@@ -24,7 +30,7 @@ S = []
 for _ in range(M):
     operation = input().split()
     if operation[0] == 'all':
-        S = [i for i in range(1, 21)]
+        S = [str(i) for i in range(1, 21)]
     elif operation[0] == 'empty':
         S = []
     else:
