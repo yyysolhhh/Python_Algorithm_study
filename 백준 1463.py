30d479458f1ec0651aa644b0c24e771138c8ccef
yyysolhhh <solbaram37@gmail.com>
2021-09-23 15:06:11 +0900

    210923 1만들기 dp

--- 백준 1463.py
+++ 백준 1463.py
@@ -4,23 +4,8 @@ N = int(input())
 dp = [0] * (N + 1)
 for i in range(2, N+1):
     dp[i] = dp[i-1] + 1
-    if N % 3 == 0:
+    if i % 3 == 0:
         dp[i] = min(dp[i], dp[i//3]+1)
-    if N % 2 == 0:
+    if i % 2 == 0:
         dp[i] = min(dp[i], dp[i//2]+1)
 print(dp[N])
-# 다른 사람 풀이
-# n = int(input())
-
-# dp = [0 for _ in range(n+1)]
-
-# for i in range(2, n+1):
-#     dp[i] = dp[i-1] + 1
-
-#     if i % 2 == 0 and dp[i] > dp[i//2] + 1:
-#         dp[i] = dp[i//2]+1
-
-#     if i % 3 == 0 and dp[i] > dp[i//3] + 1:
-#         dp[i] = dp[i//3] + 1
-
-# print(dp[n])
