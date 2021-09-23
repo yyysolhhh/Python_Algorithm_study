69cd73ba596f9754dec027abc848337036ddb250
yyysolhhh <solbaram37@gmail.com>
2021-09-24 00:44:44 +0900

    210924

--- 백준 6064.py
+++ 백준 6064.py
@@ -1,17 +1,28 @@
 T = int(input())
+# 시간 초과
+# for _ in range(T):
+#     M, N, x, y = map(int, input().split())
+#     year = min(M, N)
+#     x1 = min(M, N)
+#     y1 = min(M, N)
+#     while True:
+#         x1 += 1
+#         y1 += 1
+#         year += 1
+#         if x1 > M:
+#             x1 = 1
+#         if y1 > N:
+#             y1 = 1
+#         if x1 == x and y1 == y:
+#             print(year)
+#             break
 for _ in range(T):
+    result = -1
+
     M, N, x, y = map(int, input().split())
-    year = min(M, N)
-    x1 = min(M, N)
-    y1 = min(M, N)
-    while True:
-        x1 += 1
-        y1 += 1
-        year += 1
-        if x1 > M:
-            x1 = 1
-        if y1 > N:
-            y1 = 1
-        if x1 == x and y1 == y:
-            print(year)
+    while x <= M * N:
+        if (x - y) % N == 0:
+            result = x
             break
+        x += M
+    print(result)
