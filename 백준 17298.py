f753dba10311c8c21822ce72c1bbd63b97027a92
yyysolhhh <solbaram37@gmail.com>
2021-09-25 00:39:50 +0900

    210925 오큰수

--- 백준 17298.py
+++ 백준 17298.py
@@ -1,9 +1,19 @@
 N = int(input())
 A = list(map(int, input().split()))
 oks = [-1 for _ in range(N)]
-for i in range(N):
-    for j in range(i+1, N):
-        if A[i] < A[j]:
-            oks[i] = A[j]
-            break
-print(' '.join(map(str, oks)))
+stack = [0]
+
+# 시간 초과
+# for i in range(N):
+#     for j in range(i+1, N):
+#         if A[i] < A[j]:
+#             oks[i] = A[j]
+#             break
+
+# 스택 이용
+for i in range(1, N):
+    while stack and A[stack[-1]] < A[i]:
+        oks[stack.pop()] = A[i]
+    stack.append(i)
+
+print(*oks)
