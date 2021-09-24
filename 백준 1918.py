117b0967bc14ce43e69763a9720e7dde471964cf
yyysolhhh <solbaram37@gmail.com>
2021-09-25 04:19:59 +0900

    210925

--- 백준 1918.py
+++ 백준 1918.py
@@ -1,9 +1,11 @@
+# from os import chdir
 infix = input()
 operator = ['+', '-', '*', '/']
 stack1 = []
 stack2 = []
 stack3 = []
 postfix = []
+check = True
 for i in range(len(infix)):
     if infix[i].isalpha():
         stack1.append(infix[i])
@@ -12,13 +14,16 @@ for i in range(len(infix)):
     else:
         if infix[i] == "(":
             stack3.append(infix[i])
+            check = True
         elif infix[i] == ")":
             stack3.pop()
-    if not stack3:
+    if not stack3 and check:
         while stack1:
             postfix.append(stack1.pop(0))
         while stack2:
             postfix.append(stack2.pop())
+        check = False
     # print(stack1, stack2, stack3)
     # print(postfix)
-print(postfix)
+print(''.join(postfix))
+# (A+(B*C))-(D/E)
