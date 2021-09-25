06647d03e11f8bfa3a5646b4b384f63d79b2002e
yyysolhhh <solbaram37@gmail.com>
2021-09-26 01:41:01 +0900

    210926 숫자카드 - 맞았는데 이해 안되는 부분 있음

--- 백준 10816.py
+++ 백준 10816.py
@@ -4,6 +4,8 @@ N = int(input())
 card = sorted(map(int, input().split()))
 M = int(input())
 cardToCount = list(map(int, input().split()))
+result = {}
+
 # 시간 초과
 # result = []
 # for i in cardToCount:
@@ -24,10 +26,28 @@ def binary(i, card, start, end):
         return binary(i, card, start, mid-1)
 
 
-result = {}
+# 틀림 - 왜 틀렸지
 for i in cardToCount:
     start = 0
     end = len(card) - 1
     if i not in result:
         result[i] = binary(i, card, start, end)
 print(' '.join(map(str, result.values())))
+print(*result.values())
+print(' '.join(str(result[x]) if x in result else '0' for x in cardToCount))
+
+# 맞음
+for i in card:
+    start = 0
+    end = len(card) - 1
+    if i not in result:
+        result[i] = binary(i, card, start, end)
+print(' '.join(str(result[x]) if x in result else '0' for x in cardToCount))
+
+# 다른 방법
+for i in card:
+    if i in result:
+        result[i] += 1
+    else:
+        result[i] = 1
+print(*(result[x] if x in result else 0 for x in cardToCount))
