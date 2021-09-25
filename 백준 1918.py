ddedc91b8b666b9dca931767fe2172e56a7f9a87
yyysolhhh <solbaram37@gmail.com>
2021-09-26 00:05:36 +0900

    210926 틀림

--- 백준 1918.py
+++ 백준 1918.py
@@ -1,29 +1,45 @@
 # from os import chdir
 infix = input()
 operator = ['+', '-', '*', '/']
-stack1 = []
-stack2 = []
-stack3 = []
+# 틀림
+# stack1 = []
+# stack2 = []
+# stack3 = []
+# postfix = []
+# check = True
+# for i in range(len(infix)):
+#     if infix[i].isalpha():
+#         stack1.append(infix[i])
+#     elif infix[i] in operator:
+#         stack2.append(infix[i])
+#     else:
+#         if infix[i] == "(":
+#             stack3.append(infix[i])
+#             check = True
+#         elif infix[i] == ")":
+#             stack3.pop()
+#     if not stack3 and check:
+#         while stack1:
+#             postfix.append(stack1.pop(0))
+#         while stack2:
+#             postfix.append(stack2.pop())
+#         check = False
+
+stack = []
 postfix = []
-check = True
-for i in range(len(infix)):
-    if infix[i].isalpha():
-        stack1.append(infix[i])
-    elif infix[i] in operator:
-        stack2.append(infix[i])
+for i in infix:
+    if i.isalpha():
+        postfix.append(i)
+    elif i == '*' or i == '/':
+        stack.append(i)
+    elif i == '+' or i == '-':
+        stack.append(i)
     else:
-        if infix[i] == "(":
-            stack3.append(infix[i])
-            check = True
-        elif infix[i] == ")":
-            stack3.pop()
-    if not stack3 and check:
-        while stack1:
-            postfix.append(stack1.pop(0))
-        while stack2:
-            postfix.append(stack2.pop())
-        check = False
-    # print(stack1, stack2, stack3)
-    # print(postfix)
+        # if i == '(':
+        # stack.append(i)
+
+        if i == ')':
+            while stack:
+                postfix.append(stack.pop())
 print(''.join(postfix))
 # (A+(B*C))-(D/E)
