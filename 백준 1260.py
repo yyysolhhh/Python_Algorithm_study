515ec9268d809116e0a0198c250a9c624e7ecb07
yyysolhhh <solbaram37@gmail.com>
2021-09-27 00:44:13 +0900

    210927

--- 백준 1260.py
+++ 백준 1260.py
@@ -1,3 +1,33 @@
+from collections import deque
 N, M, V = map(int, input().split())
+graph = [[0] * (N + 1) for _ in range(N+1)]
+
 for _ in range(M):
-    point = map(int, input().split())
+    m1, m2 = map(int, input().split())
+    graph[m1][m2] = graph[m2][m1] = 1
+
+
+def bfs(start):
+    visited = [start]
+    queue = deque()
+    queue.append(start)
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for w in range(len(graph[start])):
+            if graph[v][w] == 1 and w not in visited:
+                visited.append(w)
+                queue.append(w)
+
+
+def dfs(start, visited=[]):
+    visited.append(start)
+    print(start, end=' ')
+    for w in range(len(graph[start])):
+        if graph[start][w] == 1 and w not in visited:
+            dfs(w, visited)
+
+
+dfs(V)
+print()
+bfs(V)
