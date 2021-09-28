cba6505e2ef9e5c00e3cbe616b88c71f01f3db6f
yyysolhhh <solbaram37@gmail.com>
2021-09-28 23:40:22 +0900

    210928 시간 초과

--- 백준 1193.py
+++ 백준 1193.py
@@ -1,14 +1,32 @@
 X = int(input())
-n = 1
-order = 1
-denom = 1
-numer = 1
-while order < X:
-    n += 1
-    for i in range(1, n+1):
-        if order == X:
-            break
-        denom = n + 1 - i
-        numer = i
-        order += 1
-print('%d/%d' % (denom, numer))
+# 시간 초과
+# n = 1
+# order = 1
+# denom = 1
+# numer = 1
+# while order < X:
+#     n += 1
+#     for i in range(1, n+1):
+#         if order == X:
+#             break
+#         denom = i
+#         numer = n + 1 - i
+#         order += 1
+# print('%d/%d' % (denom, numer))
+
+# 2
+line = 1
+cnt = 1
+up = 1
+down = line
+while cnt < X:
+    if (line + 1) * line / 2 <= cnt:
+        line += 1
+        up = 1
+        down = line
+        cnt += 1
+        continue
+    cnt += 1
+    up += 1
+    down -= 1
+print(up, '/', down, sep='')
