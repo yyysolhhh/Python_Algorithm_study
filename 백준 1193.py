6b0e72af8f96290ef1bd095f777d963f3737b319
yyysolhhh <solbaram37@gmail.com>
2021-09-28 21:04:04 +0900

    210928

--- 백준 1193.py
+++ 백준 1193.py
@@ -1,1 +1,14 @@
 X = int(input())
+n = 1
+order = 1
+denom = 1
+numer = 1
+while order < X:
+    n += 1
+    for i in range(1, n+1):
+        if order == X:
+            break
+        denom = n + 1 - i
+        numer = i
+        order += 1
+print('%d/%d' % (denom, numer))
