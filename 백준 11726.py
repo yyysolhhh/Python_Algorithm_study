d3aa1a5699ff936f59927df5e6a1345a68250828
yyysolhhh <solbaram37@gmail.com>
2021-09-29 23:52:55 +0900

    210929 2*n 타일링

--- 백준 11726.py
+++ 백준 11726.py
@@ -1,6 +1,25 @@
 n = int(input())
 dp = [1, 2]
-for i in range(2, n):
-    dp.append(dp[i-1] + dp[i-2])
+
+if n == 1:
+    print(dp[0] % 10007)
+elif n == 2:
+    print(dp[1] % 10007)
+else:
+    for i in range(2, n):
+        dp.append(dp[i-1] + dp[i-2])
+    print(dp[-1] % 10007)
+
+# 2
+n = int(input())
+dp = []
+
+for i in range(0, n):
+    if i == 0:
+        dp.append(1)
+    elif i == 1:
+        dp.append(2)
+    else:
+        dp.append(dp[i-1] + dp[i-2])
     print(dp)
 print(dp[-1] % 10007)
