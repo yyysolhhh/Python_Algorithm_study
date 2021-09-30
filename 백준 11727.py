530b1538c63b40b7cf92ce7ffc66a7a2093ec5af
yyysolhhh <solbaram37@gmail.com>
2021-10-01 03:35:52 +0900

    211001 타일링

--- 백준 11727.py
+++ 백준 11727.py
@@ -1,10 +1,10 @@
 n = int(input())
-dp = []
-for i in range(1, n):
+dp = [0]
+for i in range(1, n+1):
     if i == 1:
         dp.append(1)
     elif i == 2:
         dp.append(3)
     else:
-        dp.append()
+        dp.append(dp[i-1] + 2 * dp[i-2])
 print(dp[-1] % 10007)
