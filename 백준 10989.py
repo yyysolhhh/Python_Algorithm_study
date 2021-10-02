7dde96759f52b884f3ea3b8a262b0eef104cd4a8
yyysolhhh <solbaram37@gmail.com>
2021-10-03 04:02:50 +0900

    211003 메모리초과하지 않게 수 정렬

--- 백준 10989.py
+++ 백준 10989.py
@@ -1,6 +1,19 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
-num = []
+
+# 메모리 초과
+# num = []
+# for _ in range(N):
+#     num.append(int(input()))
+# for i in sorted(num):
+#     print(i)
+
+# 2
+num_list = [0 for _ in range(10001)]
 for _ in range(N):
-    num.append(int(input()))
-for i in sorted(num):
-    print(i)
+    num = int(input())
+    num_list[num] += 1
+for i in range(len(num_list)):
+    for j in range(num_list[i]):
+        print(i)
