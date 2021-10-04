6342ea1c698c1072ffe11e722c87577ffb58d720
yyysolhhh <solbaram37@gmail.com>
2021-10-05 02:54:51 +0900

    211005 덩치 비교

---
+++ 백준 7568.py
@@ -0,0 +1,13 @@
+N = int(input())
+info = []
+result = [0 for i in range(N)]
+for _ in range(N):
+    info.append(list(map(int, input().split())))
+for i in range(len(info)):
+    for j in range(len(info)):
+        if i != j:
+            if info[i][0] < info[j][0] and info[i][1] < info[j][1]:
+                result[i] += 1
+for i in range(len(result)):
+    result[i] += 1
+print(*result)
