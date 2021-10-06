a87290b692c86ea9ed493e086ebf8e15ebf6918a
yyysolhhh <solbaram37@gmail.com>
2021-10-07 02:28:12 +0900

    211007 시간초과

--- 백준 2108.py
+++ 백준 2108.py
@@ -2,32 +2,38 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 num = []
-mean, med, mod, rng = 0, 0, 0, 0
 for _ in range(N):
     num.append(int(input()))
 num.sort()
-# mean
-for i in num:
-    mean += i
-mean = mean//len(num)
-# median
-med = num[len(num)//2]
-# mode
-count = {}
-for i in num:
-    count[i] = num.count(i)
-count = sorted(count.items(), key=lambda x: (x[1], x[0]))
-if len(count) > 1:
-    if count[0][1] == count[1][1]:
-        mod = count[1][0]
+
+
+def mean():
+    return sum(num)//len(num)
+
+
+def median():
+    return num[len(num)//2]
+
+
+def mode():
+    count = {}
+    for i in num:
+        count[i] = num.count(i)
+    count = sorted(count.items(), key=lambda x: (x[1], x[0]))
+    if len(count) > 1:
+        if count[0][1] == count[1][1]:
+            return count[1][0]
+        else:
+            return count[0][0]
     else:
-        mod = count[0][0]
-else:
-    mod = count[0][0]
-# range
-rng = num[-1] - num[0]
-# print
-print(mean)
-print(med)
-print(mod)
-print(rng)
+        return count[0][0]
+
+
+def range():
+    return num[-1] - num[0]
+
+
+print(mean())
+print(median())
+print(mode())
+print(range())
