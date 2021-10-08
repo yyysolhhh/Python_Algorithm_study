19499ea4b8a6cf02d3adb702c3693e2f2ae13ecd
yyysolhhh <solbaram37@gmail.com>
2021-10-09 03:44:02 +0900

    211009 틀림

--- 백준 4949.py
+++ 백준 4949.py
@@ -1,2 +1,17 @@
-string = input()
-for i in string:
+import sys
+input = sys.stdin.readline
+while True:
+    string = input().strip()
+    if string == '.':
+        break
+    stack = [0]
+    for i in string:
+        if i == '(' or i == '[':
+            stack.append(i)
+        elif i == ')' and stack[-1] == '(' or i == ']' and stack[-1] == '[':
+            stack.pop()
+    stack.pop()
+    if len(stack):
+        print("no")
+    else:
+        print("yes")
