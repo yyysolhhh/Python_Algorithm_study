c5a95459aa97a441b9d1b1f4fc4a46a58b2d8150
yyysolhhh <solbaram37@gmail.com>
2021-10-10 02:13:16 +0900

    211010 괄호 균형

--- 백준 4949.py
+++ 백준 4949.py
@@ -1,21 +1,38 @@
 import sys
 input = sys.stdin.readline
 while True:
-    string = input().strip()
+    string = input().rstrip()
+    result = "yes"
     if string == '.':
         break
-    stack = [0]
+    stack = []
     for i in string:
         if i == '(' or i == '[':
             stack.append(i)
-        elif i == ')' and stack[-1] == '(':
-            stack.pop()
-        elif i == ']' and stack[-1] == '[':
-            stack.pop()
-        if not stack and i == ')' or i == ']':
-            break
-    stack.pop()
-    if len(stack):
-        print("no")
-    else:
-        print("yes")
+        elif i == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                result = 'no'
+                break
+        elif i == ']':
+            if stack and stack[-1] == '[':
+                stack.pop()
+            else:
+                result = 'no'
+                break
+    print(result)
+    # 틀림
+    # if i == '(' or i == '[':
+    #     stack.append(i)
+    # elif i == ')' and stack[-1] == '(':
+    #     stack.pop()
+    # elif i == ']' and stack[-1] == '[':
+    #     stack.pop()
+    # if not stack and i == ')' or i == ']':
+    #     break
+    # stack.pop()
+    # if len(stack):
+    #     print("no")
+    # else:
+    #     print("yes")
