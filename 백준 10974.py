d57b31c3cf147d3907e09be0dbea687d76b3ac9a
yyysolhhh <solbaram37@gmail.com>
2021-10-12 02:09:42 +0900

    211012 틀림

--- 백준 10974.py
+++ 백준 10974.py
@@ -1,10 +1,13 @@
 N = int(input())
 perm = [i for i in range(1, N+1)]
-for i in range(N-1, 0, -1):
-    print(*perm)
-    if perm[i] > perm[i-1]:
-        for j in range(N-1, i-1, -1):
-            if perm[i-1] < perm[j]:
-                perm[i-1], perm[j] = perm[j], perm[i-1]
-                perm[i:] = sorted(perm[i:])
-                break
+while True:
+    for i in range(N-1, 0, -1):
+        print(*perm)
+        if perm[i] > perm[i-1]:
+            for j in range(N-1, i-1, -1):
+                if perm[i-1] < perm[j]:
+                    perm[i-1], perm[j] = perm[j], perm[i-1]
+                    perm[i:] = sorted(perm[i:])
+                    break
+    if perm == sorted(perm, reverse=True):
+        break
