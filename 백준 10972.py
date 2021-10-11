5ba7c30ed6a0fc060760e3e71e10be2169d5fdde
yyysolhhh <solbaram37@gmail.com>
2021-10-11 18:11:56 +0900

    211011 틀림

--- 백준 10972.py
+++ 백준 10972.py
@@ -1,4 +1,15 @@
 N = int(input())
-permutation = list(map(int, input().split()))
-for i in range(len(permutation)):
-    if permutation[i] < permutation[i+1]:
+perm = list(map(int, input().split()))
+last = True
+for i in range(N-1, 0, -1):
+    if perm[i] > perm[i-1]:
+        for j in range(i, N):
+            if perm[i-1] < perm[j]:
+                perm[i-1], perm[j] = perm[j], perm[i-1]
+                perm[i:].sort()
+                break
+        print(*perm)
+        last = False
+        break
+if last:
+    print(-1)
