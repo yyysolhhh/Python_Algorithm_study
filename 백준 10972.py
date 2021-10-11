7c3a36a3bd26f93af1de629468fb2f98fa317aea
yyysolhhh <solbaram37@gmail.com>
2021-10-11 18:18:59 +0900

    211011 다음 순열

--- 백준 10972.py
+++ 백준 10972.py
@@ -3,10 +3,10 @@ perm = list(map(int, input().split()))
 last = True
 for i in range(N-1, 0, -1):
     if perm[i] > perm[i-1]:
-        for j in range(i, N):
+        for j in range(N-1, i-1, -1):
             if perm[i-1] < perm[j]:
                 perm[i-1], perm[j] = perm[j], perm[i-1]
-                perm[i:].sort()
+                perm[i:] = sorted(perm[i:])
                 break
         print(*perm)
         last = False
