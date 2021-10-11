7638c57ec63a4c0adaca889b4a5dc4c4c2ef9b91
yyysolhhh <solbaram37@gmail.com>
2021-10-12 00:02:31 +0900

    211012 이전 순열

--- 백준 10973.py
+++ 백준 10973.py
@@ -1,3 +1,5 @@
+# 뒤에서부터 오면서 앞 수가 더 큰 쌍을 찾고 그 쌍의 앞 수보다 작은 수를 뒤에서부터 찾아 그 앞 수와 swap하고 뒷부분을 내림차순으로 정렬한다.
+# 이전 순열을 찾으면 first = False로 바꾸고 첫 순열일 경우 -1을 출력한다.
 N = int(input())
 perm = list(map(int, input().split()))
 first = True
