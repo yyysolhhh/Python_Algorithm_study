dfa866c5c506a298a21daa1bcebae38453be7be3
yyysolhhh <solbaram37@gmail.com>
2021-10-12 20:50:52 +0900

    211012 typeerror

--- 백준 10974.py
+++ 백준 10974.py
@@ -1,13 +1,21 @@
 N = int(input())
-perm = [i for i in range(1, N+1)]
-while True:
-    for i in range(N-1, 0, -1):
-        print(*perm)
+first_perm = [i for i in range(1, N+1)]
+perm = first_perm
+
+
+def permutation(n, perm):
+    for i in range(n-1, 0, -1):
         if perm[i] > perm[i-1]:
-            for j in range(N-1, i-1, -1):
+            for j in range(n-1, i-1, -1):
                 if perm[i-1] < perm[j]:
                     perm[i-1], perm[j] = perm[j], perm[i-1]
                     perm[i:] = sorted(perm[i:])
-                    break
-    if perm == sorted(perm, reverse=True):
+                    return perm
+
+
+while True:
+    print(*perm)
+    perm = permutation(N, perm)
+    if perm == sorted(first_perm, reverse=True):
+        print(*perm)
         break
