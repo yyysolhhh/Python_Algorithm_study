88b93b34d5533c8b82eac63ea8d4a5c1f72a29c1
yyysolhhh <solbaram37@gmail.com>
2021-10-13 23:49:15 +0900

    211013

--- 백준 2805.py
+++ 백준 2805.py
@@ -1,1 +1,22 @@
+def binary(H, N, M, height):
+    while True:
+        cut = 0
+        j = 1
+        for i in range(N):
+            if height[i] - H > 0:
+                cut += height[i] - H
+        j += 1
+        if cut == M:
+            return H
+        elif cut > M:
+            H += int(H / (2 ** j))
+            binary(H, N, M, height)
+        else:
+            H -= int(H / (2 ** j))
+            binary(H, N, M, height)
+
+
 N, M = map(int, input().split())
+height = sorted(map(int, input().split()))
+H = int(height[-1] / 2)
+print(binary(H, N, M, height))
