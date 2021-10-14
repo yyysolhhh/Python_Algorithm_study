baf0e9314d3996d074400236f105d5910e90a84b
yyysolhhh <solbaram37@gmail.com>
2021-10-15 02:38:17 +0900

    211015 비밀번호 찾기

--- 백준 17219.py
+++ 백준 17219.py
@@ -1,6 +1,7 @@
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
+# 1
 site = []
 add = []
 for _ in range(N):
@@ -9,3 +10,11 @@ site_dict = dict(site)
 for _ in range(M):
     add = input().rstrip()
     print(site_dict[add])
+# 2
+site_info = {}
+for _ in range(N):
+    add, pss = input().split()
+    site_info[add] = pss
+for _ in range(M):
+    site = input().rstrip()
+    print(site_info[site])
