c1cff777cf4825ffe458495ed50215b53caf6dc5
yyysolhhh <solbaram37@gmail.com>
2021-10-17 02:34:23 +0900

    211017 듣보잡

--- 백준 1764.py
+++ 백준 1764.py
@@ -1,16 +1,25 @@
 import sys
 input = sys.stdin.readline
+
 N, M = map(int, input().split())
 not_heard = set()
 not_seen = set()
 result = set()
+
 for _ in range(N):
     not_heard.add(input().rstrip())
+
 for _ in range(M):
     not_seen.add(input().rstrip())
-for i in not_heard:
-    if i in not_seen:
-        result.add(i)
+
+# 1
+# for i in not_heard:
+#     if i in not_seen:
+#         result.add(i)
+
+# 2 교집합
+result = not_heard & not_seen
+
 print(len(result))
 for i in sorted(result):
     print(i)
