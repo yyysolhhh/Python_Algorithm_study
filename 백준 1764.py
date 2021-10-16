cb351eac4a19e027a430b12d457e154fc4ba4def
yyysolhhh <solbaram37@gmail.com>
2021-10-17 02:30:47 +0900

    211017

--- 백준 1764.py
+++ 백준 1764.py
@@ -1,16 +1,16 @@
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
-not_heard = []
-not_seen = []
-result = []
+not_heard = set()
+not_seen = set()
+result = set()
 for _ in range(N):
-    not_heard.append(input().rstrip())
+    not_heard.add(input().rstrip())
 for _ in range(M):
-    not_seen.append(input().rstrip())
+    not_seen.add(input().rstrip())
 for i in not_heard:
     if i in not_seen:
-        result.append(i)
+        result.add(i)
 print(len(result))
 for i in sorted(result):
     print(i)
