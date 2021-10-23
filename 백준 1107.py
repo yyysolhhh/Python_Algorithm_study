5314683c3e5479e279e439c3d6ad444440ef867e
yyysolhhh <solbaram37@gmail.com>
2021-10-23 18:48:57 +0900

    211023 리모컨

--- 백준 1107.py
+++ 백준 1107.py
@@ -1,15 +1,14 @@
-N = int(input())
-M = int(input())
-if M:
+N = int(input())    # target
+M = int(input())    # broken button 개수
+if M:   # M > 0일 때만 입력 받음
     broken = set(input().split())
 else:
     broken = set()
-# channel = 100
-result = abs(N - 100)
-for i in range(1000000):
-    for j in str(i):
+result = abs(N - 100)   # +나 -로만 이동하는 경우 - 최댓값
+for i in range(1000000):    # 작은 수에서 +로 올라가는 경우, 큰 수에서 -로 내려오는 경우까지 고려
+    for j in str(i):    # i의 자릿수 중 broken이 있는 경우 중단
         if j in broken:
             break
-    else:
+    else:   # i가 버튼을 다 누를 수 있는 수인 경우 기존 답과 i의 자릿수 + i에서 target까지 거리 중 적은 수로 갱신
         result = min(result, len(str(i)) + abs(N - i))
 print(result)
