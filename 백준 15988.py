032a1fd3efec075d49a61cde8939a8798625864c
yyysolhhh <solbaram37@gmail.com>
2021-10-26 02:37:56 +0900

    211026

---
+++ 백준 15988.py
@@ -0,0 +1,15 @@
+def num_of_case(n):   # 9095 참고
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return num_of_case(n-1) + num_of_case(n-2) + num_of_case(n-3)
+
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    print(num_of_case(n) % 1000000009)
