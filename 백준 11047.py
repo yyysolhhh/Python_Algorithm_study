22ee10406189e6ed3018ef16f8a4c6670893ffdc
yyysolhhh <solbaram37@gmail.com>
2021-10-26 22:54:49 +0900

    211026 동전 그리디

--- 백준 11047.py
+++ 백준 11047.py
@@ -2,13 +2,22 @@ N, K = map(int, input().split())
 A = []
 for _ in range(N):
     A.append(int(input()))
-sum, num = 0, 0
+# 1 시간초과
+# sum, num = 0, 0
+# for i in list(reversed(A)):
+#     if i < K:
+#         while sum <= K:
+#             if sum + i > K:
+#                 break
+#             sum += i
+#             num += 1
+# print(num)
+# 2
+res = 0
 for i in list(reversed(A)):
-    if i < K:
-        while sum <= K:
-            if sum + i > K:
-                break
-            sum += i
-            num += 1
-            # print(i, sum, num)
-print(sum, num)
+    res += K // i
+    K %= i
+    if K == 0:
+        break
+    print(i, K)
+print(res)
