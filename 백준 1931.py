f82a30516fe2d23278f3940fa661d1e7f1b5e809
yyysolhhh <solbaram37@gmail.com>
2021-10-27 21:49:10 +0900

    211027 회의실 배정 그리디

--- 백준 1931.py
+++ 백준 1931.py
@@ -3,17 +3,26 @@ info = []
 for _ in range(N):
     start, end = map(int, input().split())
     info.append([start, end])
-info.sort(key=lambda x: (x[0], x[1]))
-res = 0
-print(info)
+
+# 1 시간 초과, 틀림
+# res = 0
+# info.sort(key=lambda x: (x[0], x[1]))
+# for i in range(N):
+#     cnt = 1
+#     pre = info[i][1]
+#     for j in range(i+1, N):
+#         if pre <= info[j][0]:
+#             pre = info[j][1]
+#             cnt += 1
+#     res = max(res, cnt)
+# print(res)
+
+# 2   빨리 시작하는 순서가 아니라 빨리 끝나는 순서대로 정렬 (빨리 끝날수록 다음 회의가 많아지기 때문)
+info.sort(key=lambda x: (x[1], x[0]))
+pre = 0
+cnt = 0
 for i in range(N):
-    cnt = 1
-    pre = info[i][1]
-    for j in range(i+1, N):
-        print(info[i], info[j], cnt)
-        if pre <= info[j][0]:
-            pre = info[j][1]
-            cnt += 1
-            print('!')
-    res = max(res, cnt)
-print(res)
+    if info[i][0] >= pre:
+        pre = info[i][1]
+        cnt += 1
+print(cnt)
