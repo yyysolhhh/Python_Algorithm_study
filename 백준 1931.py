7f43cdc7ddaa969765eee67a7cd583dec469cc94
yyysolhhh <solbaram37@gmail.com>
2021-10-27 15:50:14 +0900

    211027 시간초과

--- 백준 1931.py
+++ 백준 1931.py
@@ -4,4 +4,16 @@ for _ in range(N):
     start, end = map(int, input().split())
     info.append([start, end])
 info.sort(key=lambda x: (x[0], x[1]))
-for i in info:
+res = 0
+print(info)
+for i in range(N):
+    cnt = 1
+    pre = info[i][1]
+    for j in range(i+1, N):
+        print(info[i], info[j], cnt)
+        if pre <= info[j][0]:
+            pre = info[j][1]
+            cnt += 1
+            print('!')
+    res = max(res, cnt)
+print(res)
