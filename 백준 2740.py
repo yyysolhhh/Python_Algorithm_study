b45cd107188de014eaffede6eb601d76cd55415b
yyysolhhh <solbaram37@gmail.com>
2021-10-29 01:45:18 +0900

    211029 행렬 곱셈

--- 백준 2740.py
+++ 백준 2740.py
@@ -1,1 +1,15 @@
 N, M = map(int, input().split())
+A = []
+B = []
+for _ in range(N):
+    A.append(list(map(int, input().split())))
+M, K = map(int, input().split())
+for _ in range(M):
+    B.append(list(map(int, input().split())))
+res = [[0 for _ in range(K)] for _ in range(N)]
+for n in range(N):
+    for k in range(K):
+        for m in range(M):
+            res[n][k] += A[n][m] * B[m][k]
+for i in range(N):
+    print(*res[i])
