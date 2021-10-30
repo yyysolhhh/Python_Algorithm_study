c7e29c256b94cb2c4683fd292e83322e6481f381
yyysolhhh <solbaram37@gmail.com>
2021-10-31 03:18:35 +0900

    211031 차이를 최대로

--- 백준 10819.py
+++ 백준 10819.py
@@ -1,7 +1,7 @@
 from itertools import permutations
 N = int(input())
 A = sorted(map(int, input().split()))
-per_A = list(permutations(A, N))
+per_A = list(permutations(A, N))  # A 원소 순서의 모든 경우를 구함
 res = 0
 for i in per_A:
     temp = 0
