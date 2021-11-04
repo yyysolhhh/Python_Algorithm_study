c2cc84f76ea30408a3cc24d99d3437c97069db9c
yyysolhhh <solbaram37@gmail.com>
2021-11-04 23:43:19 +0900

    211104 16진수->10진수

---
+++ 백준 1550.py
@@ -0,0 +1,11 @@
+# 1
+# num = input()
+# decimal = 0
+# for i in range(len(num)):
+#     if num[i].isalpha():
+#         decimal += (ord(num[i]) - 55) * 16 ** (len(num) - i - 1)
+#     else:
+#         decimal += int(num[i]) * 16 ** (len(num) - i - 1)
+# print(decimal)
+# 2
+print(int(input(), 16))
