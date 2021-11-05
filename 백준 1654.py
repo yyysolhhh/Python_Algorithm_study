b127f3b7af9212b00368b66d84c937f47b7b499c
yyysolhhh <solbaram37@gmail.com>
2021-11-06 03:38:47 +0900

    211106 랜선 자르기 이분 탐색

--- 백준 1654.py
+++ 백준 1654.py
@@ -2,3 +2,18 @@ K, N = map(int, input().split())
 length = []
 for _ in range(K):
     length.append(int(input()))
+length.sort()
+short = length[0]
+start = 1
+end = short
+while start <= end:
+    num = 0
+    measure = (start + end) // 2
+    for i in length:
+        num += i // measure
+    if num >= N:
+        start = measure + 1
+    else:
+        end = measure - 1
+    # print(measure, num)
+print(end)
