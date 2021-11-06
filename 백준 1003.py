f976c58f9c54401fa66522c3317d8807f7cb3c52
yyysolhhh <solbaram37@gmail.com>
2021-11-07 05:05:58 +0900

    211107

--- 백준 1003.py
+++ 백준 1003.py
@@ -1,17 +1,17 @@
 def fibonacci(n, cnt0, cnt1):
     if n == 0:
         cnt0 += 1
-        return cnt0, cnt1
+        # return cnt0, cnt1
     elif n == 1:
         cnt1 += 1
-        return cnt0, cnt1
+        # return cnt0, cnt1
     else:
         return fibonacci(n-1, cnt0, cnt1) + fibonacci(n-2, cnt0, cnt1)
 
 
 T = int(input())
+cnt0 = 0
+cnt1 = 0
 for _ in range(T):
     N = int(input())
-    cnt0 = 0
-    cnt1 = 0
     print(fibonacci(N, cnt0, cnt1))
