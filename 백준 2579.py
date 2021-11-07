4369771d4cb8f7739c91d48a83e44e5f031b7675
yyysolhhh <solbaram37@gmail.com>
2021-11-08 02:08:53 +0900

    211108

--- 백준 2579.py
+++ 백준 2579.py
@@ -2,3 +2,10 @@ n = int(input())
 stairs = []
 for _ in range(n):
     stairs.append(int(input()))
+dp = [0 for _ in range(n+1)]
+dp[1] = stairs[n-1]
+print(stairs)
+for i in range(2, n+1):
+    dp[i] = max(stairs[n-i] + dp[i-1], stairs[n-i] + dp[i-2])
+    print(dp)
+print(dp[n])
