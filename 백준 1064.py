41886b2e1832925ebda4c6622d373f2da54a5c88
yyysolhhh <solbaram37@gmail.com>
2021-11-11 04:11:25 +0900

    211111 평행사변형

--- 백준 1064.py
+++ 백준 1064.py
@@ -1,1 +1,20 @@
 xa, ya, xb, yb, xc, yc = map(int, input().split())
+if (xb - xa) * (yc - ya) == (xc - xa) * (yb - ya):
+    print(-1.0)
+else:
+    side1 = ((xa - xb) ** 2 + (ya - yb) ** 2) ** 0.5
+    side2 = ((xb - xc) ** 2 + (yb - yc) ** 2) ** 0.5
+    side3 = ((xa - xc) ** 2 + (ya - yc) ** 2) ** 0.5
+    sides = sorted([side1, side2, side3])
+    # 1
+    # length = []
+    # for i in range(3):
+    #     for j in range(3):
+    #         if i == j:
+    #             break
+    #         length.append((sides[i] + sides[j]) * 2)
+    # print(max(length) - min(length))
+    # 2
+    max_len = (sides[2] + sides[1]) * 2
+    min_len = (sides[1] + sides[0]) * 2
+    print(max_len - min_len)
