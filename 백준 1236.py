198df9517f276d2f9ca6f17f52cd1f8c88665d58
yyysolhhh <solbaram37@gmail.com>
2021-11-12 02:49:35 +0900

    211112

--- 백준 1236.py
+++ 백준 1236.py
@@ -2,11 +2,30 @@ N, M = map(int, input().split())
 castle = []
 for _ in range(N):
     castle.append(list(input()))
-res = 0
+
+# 1 틀림
+# res = 0
+# for i in range(N):
+#     if 'X' not in castle[i]:
+#         for j in range(M):
+#             if 'X' not in castle[:][j]:
+#                 castle[i][j] = 'X'
+#                 res += 1
+#                 print(i, j)
+#                 break
+#         else:
+#             castle[i][0] = 'X'
+#             res += 1
+#             print(i, j)
+# print(res)
+
+# 2 X가 없는 행, 열의 개수 중 더 큰 값을 출력
+row = 0
+col = 0
 for i in range(N):
     if 'X' not in castle[i]:
-        for j in range(M):
-            if 'X' not in castle[:][j]:
-                castle[i][j] = 'X'
-                res += 1
-print(res // 2)
+        row += 1
+for j in range(M):
+    if 'X' not in (castle[i][j] for i in range(N)):
+        col += 1
+print(max(row, col))
