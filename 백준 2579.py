af6d7310b822527f3d0faebff1232c83227944ee
yyysolhhh <solbaram37@gmail.com>
2021-11-14 01:24:48 +0900

    211114 계단오르기 indexerror

--- 백준 2579.py
+++ 백준 2579.py
@@ -2,9 +2,10 @@ n = int(input())
 stairs = []
 for _ in range(n):
     stairs.append(int(input()))
-dp = [0 for _ in range(n+1)]
-
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + max(stairs[i-1], stairs[i-2])
-    print(dp)
-print(dp[n])
+dp = []
+dp.append(stairs[0])
+dp.append(stairs[0] + stairs[1])
+dp.append(max(stairs[0] + stairs[2], stairs[1] + stairs[2]))
+for i in range(3, n):
+    dp.append(max(dp[i-3] + stairs[i-1] + stairs[i], dp[i-2] + stairs[i]))
+print(dp[n-1])
