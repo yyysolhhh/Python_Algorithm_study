306ecd62ddd558cdb94003e1e72575784d9eb3ef
yyysolhhh <solbaram37@gmail.com>
2021-11-17 01:26:23 +0900

    211117

--- 백준 2480.py
+++ 백준 2480.py
@@ -1,14 +1,21 @@
 die0, die1, die2 = list(map(int, input().split()))
+# if die0 == die1 == die2:
+#     print(10000 + die0 * 1000)
+# elif die0 != die1 != die2:
+#     print(max(die0, die1, die2) * 100)
+# else:
+#     if die0 == die1:
+#         print(1000 + die0 * 100)
+#     elif die1 == die2:
+#         print(1000 + die1 * 100)
+#     else:
+#         print(1000 + die0 * 100)
+
 if die0 == die1 == die2:
     print(10000 + die0 * 1000)
-elif die0 != die1 != die2:
-    print(max(die0, die1, die2) * 100)
+elif die0 == die1 or die0 == die2:
+    print(1000 + die0 * 100)
+elif die1 == die2:
+    print(1000 + die1 * 100)
 else:
-    if die0 == die1:
-        print(1000 + die0 * 100)
-    elif die1 == die2:
-        print(1000 + die1 * 100)
-    else:
-        print(1000 + die0 * 100)
-# elif die0 == die1 != die2 or die1 == die2 != die0 or die0 == die2 != die1:
-        # print(1000 + )
+    print(max(die0, die1, die2) * 100)
