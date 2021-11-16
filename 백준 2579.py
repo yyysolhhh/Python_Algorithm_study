32a361f4a534efe8027c34b0b6196232a903a1de
yyysolhhh <solbaram37@gmail.com>
2021-11-17 00:42:43 +0900

    211117

--- 백준 2579.py
+++ 백준 2579.py
@@ -1,11 +1,24 @@
+import sys
+input = sys.stdin.readline
 n = int(input())
 stairs = []
 for _ in range(n):
     stairs.append(int(input()))
+
+# 1
 dp = []
 dp.append(stairs[0])
 dp.append(stairs[0] + stairs[1])
 dp.append(max(stairs[0] + stairs[2], stairs[1] + stairs[2]))
 for i in range(3, n):
     dp.append(max(dp[i-3] + stairs[i-1] + stairs[i], dp[i-2] + stairs[i]))
+print(dp[n-1])
+
+# 2
+dp = [0 for _ in range(n)]
+dp[0] = stairs[0]
+dp[1] = stairs[0] + stairs[1]
+dp[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2])
+for i in range(3, n):
+    dp[i] = max(dp[i-3] + stairs[i-1] + stairs[i], dp[i-2] + stairs[i])
 print(dp[n-1])
