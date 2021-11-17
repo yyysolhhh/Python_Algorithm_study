33e84b9d7aeabab214a94e0fd9b05989cbed5ae3
yyysolhhh <solbaram37@gmail.com>
2021-11-17 21:58:37 +0900

    211117 팩토리얼 시간초과

--- 백준 1564.py
+++ 백준 1564.py
@@ -2,17 +2,12 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 factorial = 1
-last = True
 cnt = 0
-res = ''
 for i in range(2, N+1):
     factorial *= i
 for i in str(factorial)[::-1]:
     if i != '0':
-        last = False
-    if i != '0':
-        res += i
-        cnt += 1
-    if cnt == 5:
         break
-print(res[::-1])
+    cnt += 1
+factorial //= 10 ** cnt
+print(str(factorial)[-5:])
