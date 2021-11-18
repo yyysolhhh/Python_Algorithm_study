3c514f23da438a7d9e5f43fffcef91dcfdb4e759
yyysolhhh <solbaram37@gmail.com>
2021-11-19 00:11:17 +0900

    211119

--- 백준 1564.py
+++ 백준 1564.py
@@ -1,12 +1,17 @@
+import math
 import sys
 input = sys.stdin.readline
 N = int(input())
-factorial = 1
-for i in range(2, N+1):
-    factorial *= i
-    # 3 시간 초과
-    while factorial % 10 == 0:
-        factorial //= 10
+# 2
+factorial = math.factorial(N)
+print(factorial)
+# 1
+# factorial = 1
+# for i in range(2, N+1):
+#     factorial *= i
+# 3 시간 초과
+# while factorial % 10 == 0:
+#     factorial //= 10
 # 1 시간 초과
 # cnt = 0
 # for i in str(factorial)[::-1]:
@@ -15,8 +20,8 @@ for i in range(2, N+1):
 #     cnt += 1
 # factorial //= 10 ** cnt
 # 2 시간 초과
-# while factorial % 10 == 0:
-#     factorial //= 10
+while factorial % 10 == 0:
+    factorial //= 10
 print(str(factorial)[-5:])
 
 # factorial %= 100000
