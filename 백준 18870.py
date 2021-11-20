fb50634c4ecb3817c84313a253dab015362fe51f
yyysolhhh <solbaram37@gmail.com>
2021-11-21 02:17:56 +0900

    211121 시간초과

--- 백준 18870.py
+++ 백준 18870.py
@@ -1,11 +1,18 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
 X = list(map(int, input().split()))
 Xp = []
-for i in range(N):
-    temp = 0
-    for j in set(X):
-        if X[i] > j:
-            temp += 1
-    Xp.append(temp)
+
+# 1 시간 초과
+# for i in range(N):
+#     temp = 0
+#     for j in set(X):
+#         if X[i] > j:
+#             temp += 1
+#     Xp.append(temp)
+
+for i in X:
+    Xp.append(sorted(set(X)).index(i))
 
 print(*Xp)
