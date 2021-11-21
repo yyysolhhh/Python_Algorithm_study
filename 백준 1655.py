1d5f56c6b22e2276dbdd532a927d1548420c0c50
yyysolhhh <solbaram37@gmail.com>
2021-11-22 01:03:35 +0900

    211122 시간초과

--- 백준 1655.py
+++ 백준 1655.py
@@ -2,20 +2,11 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 baek = []
-queue = []
 for _ in range(N):
     baek.append(int(input()))
 
 # 1 시간 초과
-# for i in range(len(baek)):
-#     queue.append(baek.pop(0))
-#     queue.sort()
-#     if len(queue) % 2 == 0:
-#         print(queue[len(queue)//2-1])
-#     else:
-#         print(queue[len(queue)//2])
-
-# 2
+queue = []
 for i in range(len(baek)):
     queue.append(baek.pop(0))
     queue.sort()
@@ -23,3 +14,5 @@ for i in range(len(baek)):
         print(queue[len(queue)//2-1])
     else:
         print(queue[len(queue)//2])
+
+# 2
