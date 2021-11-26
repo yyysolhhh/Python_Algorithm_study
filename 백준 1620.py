4806ed97352eb5935dcaa48e4d55a71a2f443bac
yyysolhhh <solbaram37@gmail.com>
2021-11-26 21:14:11 +0900

    211126 시간초과

--- 백준 1620.py
+++ 백준 1620.py
@@ -1,11 +1,12 @@
+import sys
+input = sys.stdin.readline
 N, M = map(int, input().split())
 pokemon = {}
 for i in range(1, N+1):
-    pokemon[i] = input()
+    pokemon[i] = input().rstrip()
 for _ in range(M):
-    Q = input()
-    if Q.isalnum():
+    Q = input().rstrip()
+    if Q.isdigit():
         print(pokemon[int(Q)])
     elif Q.isalpha():
-        # print(pokemon.keys(Q))
-        print('q')
+        print(*[i for i, j in pokemon.items() if j == Q])
