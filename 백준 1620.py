ef255c9fdd5b4a98af9808b70d36161def0b137a
yyysolhhh <solbaram37@gmail.com>
2021-11-26 21:55:10 +0900

    211126 포켓몬 딕셔너리

--- 백준 1620.py
+++ 백준 1620.py
@@ -1,12 +1,27 @@
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
-pokemon = {}
+
+# 1 시간 초과
+# pokemon = {}
+# for i in range(1, N+1):
+#     pokemon[i] = input().rstrip()
+# for _ in range(M):
+#     Q = input().rstrip()
+#     if Q.isdigit():
+#         print(pokemon[int(Q)])
+#     elif Q.isalpha():
+#         print(*[i for i, j in pokemon.items() if j == Q])
+# 2 맞음
+poke_name = []
+poke_num = {}
 for i in range(1, N+1):
-    pokemon[i] = input().rstrip()
+    pokemon = input().rstrip()
+    poke_name.append(pokemon)
+    poke_num[pokemon] = i
 for _ in range(M):
     Q = input().rstrip()
     if Q.isdigit():
-        print(pokemon[int(Q)])
+        print(poke_name[int(Q)-1])
     elif Q.isalpha():
-        print(*[i for i, j in pokemon.items() if j == Q])
+        print(poke_num[Q])
