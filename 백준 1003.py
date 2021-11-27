a6d124f586adcc1043ac089ab0d4caff4e8126b2
yyysolhhh <solbaram37@gmail.com>
2021-11-27 23:57:15 +0900

    211127 피보나치 함수 dp

--- 백준 1003.py
+++ 백준 1003.py
@@ -1,17 +1,24 @@
-def fibonacci(n, cnt0, cnt1):
-    if n == 0:
-        cnt0 += 1
-        # return cnt0, cnt1
-    elif n == 1:
-        cnt1 += 1
-        # return cnt0, cnt1
-    else:
-        return fibonacci(n-1, cnt0, cnt1) + fibonacci(n-2, cnt0, cnt1)
+# 문제 코드
+# def fibonacci(n):
+#     if n == 0:
+#         print(0)
+#         return 0
+#     elif n == 1:
+#         print(1)
+#         return 1
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)
 
 
 T = int(input())
-cnt0 = 0
-cnt1 = 0
+dp = [[0] * 2 for _ in range(41)]
+dp[0][0] = 1
+dp[0][1] = 0
+dp[1][0] = 0
+dp[1][1] = 1
+for i in range(2, 41):
+    dp[i][0] = dp[i-1][0] + dp[i-2][0]
+    dp[i][1] = dp[i-1][1] + dp[i-2][1]
 for _ in range(T):
     N = int(input())
-    print(fibonacci(N, cnt0, cnt1))
+    print(dp[N][0], dp[N][1])
