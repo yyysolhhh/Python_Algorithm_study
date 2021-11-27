0a021c8d8e8a23a8c9ff7dc7cc28a32135933b56
yyysolhhh <solbaram37@gmail.com>
2021-11-27 23:13:19 +0900

    211127 계단오르기 dp

--- 백준 2579.py
+++ 백준 2579.py
@@ -1,11 +1,11 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-stairs = []
-for _ in range(n):
-    stairs.append(int(input()))
+stairs = [0 for _ in range(n+2)]
+for i in range(n):
+    stairs[i] = int(input())
 
-# 1
+# 1 맞음
 dp = []
 dp.append(stairs[0])
 dp.append(stairs[0] + stairs[1])
@@ -14,8 +14,8 @@ for i in range(3, n):
     dp.append(max(dp[i-3] + stairs[i-1] + stairs[i], dp[i-2] + stairs[i]))
 print(dp[n-1])
 
-# 2
-dp = [0 for _ in range(n)]
+# 2 맞음
+dp = [0 for _ in range(n+2)]
 dp[0] = stairs[0]
 dp[1] = stairs[0] + stairs[1]
 dp[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2])
