deea17b65507a4f8aafdb5aa49caef6e20964eab
yyysolhhh <solbaram37@gmail.com>
2021-11-28 23:58:09 +0900

    211128

--- 백준 5525.py
+++ 백준 5525.py
@@ -3,7 +3,7 @@ M = int(input())
 S = input()
 PN = 'IO' * N + 'I'
 cnt = 0
-for i in range(len(S) - len(PN)):
+for i in range(M - len(PN)):
     if S[i:i+len(PN)] == PN:
         cnt += 1
 print(cnt)
