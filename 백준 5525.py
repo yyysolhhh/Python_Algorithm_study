411261cd3694dcb6af318b9e5cfc35a050b01834
yyysolhhh <solbaram37@gmail.com>
2021-11-29 23:47:35 +0900

    211129

--- 백준 5525.py
+++ 백준 5525.py
@@ -1,9 +1,24 @@
 N = int(input())
 M = int(input())
 S = input()
-PN = 'IO' * N + 'I'
+
+# 2
 cnt = 0
-for i in range(M - len(PN)):
-    if S[i:i+len(PN)] == PN:
-        cnt += 1
-print(cnt)
+I = 0
+PN = 0
+while i <= N:
+    if S[i-1] == 'I' and S[i] == 'O' and S[i+1] == 'I':
+        i += 1
+        if i == N:
+            PN += 1
+    else:
+        i = 0
+
+
+# 1 시간 초과
+# PN = 'IO' * N + 'I'
+# cnt = 0
+# for i in range(M - len(PN)):
+#     if S[i:i+len(PN)] == PN:
+#         cnt += 1
+# print(cnt)
