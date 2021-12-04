7e2de630d410a2404785abcf8652a1b2ff4232bf
yyysolhhh <solbaram37@gmail.com>
2021-12-05 00:05:00 +0900

    211205 최대 힙

--- 백준 11279.py
+++ 백준 11279.py
@@ -1,12 +1,29 @@
+import sys
+import heapq
+input = sys.stdin.readline
 N = int(input())
-arr = []
+
+# 1 시간초과
+# arr = []
+# for _ in range(N):
+#     x = int(input())
+#     if x > 0:
+#         arr.append(x)
+#     elif x == 0:
+#         if arr:
+#             print(max(arr))
+#             arr.remove(max(arr))
+#         else:
+#             print(0)
+
+# 2 heapq 모듈 사용
+heap = []
 for _ in range(N):
     x = int(input())
     if x > 0:
-        arr.append(x)
+        heapq.heappush(heap, (-x, x))
     elif x == 0:
-        if arr:
-            print(max(arr))
-            arr.remove(max(arr))
+        if heap:
+            print(heapq.heappop(heap)[1])
         else:
             print(0)
