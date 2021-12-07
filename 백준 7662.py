67d3745dcc9b9fb1eff47be41cbe80ed0f5edddc
yyysolhhh <solbaram37@gmail.com>
2021-12-07 23:51:01 +0900

    211207 시간초과

--- 백준 7662.py
+++ 백준 7662.py
@@ -14,16 +14,19 @@ for _ in range(T):
             visited[i] = True
         elif op == "D":   # 삭제
             if num == 1:    # 최댓값 삭제
-
+                while max_heap and not visited[max_heap[0][1]]:
+                    heapq.heappop(max_heap)
                 if max_heap:
                     visited[max_heap[0][1]] = False
                     heapq.heappop(max_heap)
             elif num == -1:   # 최솟값 삭제
+                while min_heap and not visited[min_heap[0][1]]:
+                    heapq.heappop(min_heap)
                 if min_heap:
                     visited[min_heap[0][1]] = False
                     heapq.heappop(min_heap)
         print(max_heap, min_heap, visited)
-    if max_heap and min_heap and max_heap[0][1] != min_heap[0]:
-        print(max_heap[0][1], min_heap[0])
+    if max_heap and min_heap:
+        print(-max_heap[0][0], min_heap[0][0])
     else:
         print('EMPTY')
