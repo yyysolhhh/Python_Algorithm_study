0840b9f12605f820d1b8f4dd7e8e3ab37368cdd8
yyysolhhh <solbaram37@gmail.com>
2021-12-09 00:25:42 +0900

    211209 귀걸이

--- 백준 1380.py
+++ 백준 1380.py
@@ -1,9 +1,20 @@
+scenario = 1
 while True:
     n = int(input())
     if n == 0:
         break
     name = []
+    mark_list = list(False for _ in range(n))
     for _ in range(n):
         name.append(input())
-    for _ in range(2*n-1):
+    for i in range(2*n-1):
         num, mark = input().split()
+        num = int(num)
+        if mark_list[num-1] == False:
+            mark_list[num-1] = True
+        else:
+            mark_list[num-1] = False
+    for i in range(len(mark_list)):
+        if mark_list[i]:
+            print(scenario, name[i])
+    scenario += 1
