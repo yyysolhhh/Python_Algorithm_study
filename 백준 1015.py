b0cd923a0ffb0891af0c650526daa79c2800f057
yyysolhhh <solbaram37@gmail.com>
2021-12-10 19:46:21 +0900

    211210

--- 백준 1015.py
+++ 백준 1015.py
@@ -1,6 +1,15 @@
 N = int(input())
 A = list(map(int, input().split()))
-P = []
+# P = {}
+# for i in range(N):
+#     P[A[i]] = i
+# print(P)
+# P = sorted(P.items())
+# print(P)
+P = [0 for _ in range(N)]
+A2 = sorted(A)
 for i in range(N):
-
+    for j in range(N):
+        if A[i] == A2[j]:
+            P[i] = j
 print(*P)
