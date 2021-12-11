b0ba95ea3df9f498ab38942c86c972a4bfd01f60
yyysolhhh <solbaram37@gmail.com>
2021-12-12 04:17:45 +0900

    211212 숫자 정사각형

--- 백준 1051.py
+++ 백준 1051.py
@@ -1,19 +1,41 @@
 N, M = map(int, input().split())
 rect = []
-side = min(N, M)
 for _ in range(N):
     rect.append(list(map(int, input())))
-ans = False
-while not ans:
-    if side == 1:
-        print(1)
-        break
-    else:
-        for i in range(N-side+1):
-            for j in range(M-side+1):
-                if rect[i][j] == rect[i][j+side-1] and rect[i][j] == rect[i+side-1][j] and rect[i][j] == rect[i+side-1][j+side-1]:
-                    print(side * side)
-                    ans = True
-                    break
-            break
-        side -= 1
+
+# 1 틀림
+# side = min(N, M)
+# check = False
+# while not check:
+#     for i in range(N-side+1):
+#         for j in range(M-side+1):
+#             if rect[i][j] == rect[i][j+side-1] == rect[i+side-1][j] == rect[i+side-1][j+side-1]:
+#                 ans = side * side
+#                 print(ans)
+#                 check = True
+#                 break
+#         break
+#     side -= 1
+
+# 2 맞음
+ans = 0
+for side in range(1, min(N, M)+1):
+    for i in range(N):
+        for j in range(M):
+            if (i + side <= N) and (j + side <= M) and (rect[i][j] == rect[i][j+side-1] == rect[i+side-1][j] == rect[i+side-1][j+side-1]):
+                ans = max(side * side, ans)
+print(ans)
+
+# 3 2번에서 side 큰것부터 - 틀림
+# ans = 1
+# for side in range(min(N, M)+1, 0, -1):
+#     for i in range(N):
+#         for j in range(M):
+#             if (i + side <= N) and (j + side <= M) and (rect[i][j] == rect[i][j+side-1] == rect[i+side-1][j] == rect[i+side-1][j+side-1]):
+#                 ans = side * side
+#                 break
+#         else:
+#             break
+#     else:
+#         break
+# print(ans)
