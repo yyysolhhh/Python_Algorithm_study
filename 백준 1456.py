42e520f14ec434c29a5a01f599fcae2a0f63afdf
yyysolhhh <solbaram37@gmail.com>
2021-12-16 01:23:39 +0900

    211216

--- 백준 1456.py
+++ 백준 1456.py
@@ -1,19 +1,70 @@
-def check_prime(n):
-    for i in range(1, int(n**0.5)):
-        if n % i == 0:
-            return False
-    print(n)
-    return True
+# 1 틀림
+# def check_prime(n):
+#     for i in range(2, int(n ** 0.5)):
+#         if n % i == 0:
+#             return False
+#     print(n)
+#     return True
 
 
+# A, B = map(int, input().split())
+# almst_prime = []
+# cnt = 0
+# for i in range(2, int(B ** 0.5)+1):
+#     if check_prime(i):
+#         for j in range(2, B):
+#             if i ** j > B:
+#                 break
+#             almst_prime.append(i ** j)
+# print(set(almst_prime))
+# for i in set(almst_prime):
+#     if A <= i and i <= B:
+#         cnt += 1
+# print(cnt)
+
+# 2 - 1 메모리 초과
 A, B = map(int, input().split())
-prime_num = []
-cnt = 0
-for i in range(1, B):
-    if check_prime(i):
-        prime_num.append(i ** 2)
-print(prime_num)
-for i in prime_num:
-    if A <= i and i <= B:
-        cnt += 1
-print(cnt)
+arr = [False] * 2 + [True for _ in range(2, B+1)]
+primes = []
+almst_prime = []
+for i in range(2, B+1):
+    if arr[i]:
+        primes.append(i)
+        for j in range(i+i, B+1, i):
+            arr[j] = False
+for i in primes:
+    for j in range(2, B):
+        if i ** j > B:
+            break
+        almst_prime.append(i ** j)
+print(len(set(almst_prime)))
+
+# 2 - 1 - 2
+A, B = map(int, input().split())
+arr = [False] * 2 + [True for _ in range(2, B+1)]
+primes = []
+almst_prime = []
+for i in range(2, B+1):
+    if arr[i]:
+        primes.append(i)
+        for j in range(i+i, B+1, i):
+            arr[j] = False
+for i in primes:
+    for j in range(2, B):
+        if i ** j > B:
+            break
+        almst_prime.append(i ** j)
+print(len(set(almst_prime)))
+
+# 2 - 2
+# A, B = map(int, input().split())
+# arr = [False] * 2 + [True for _ in range(2, B+1)]
+# primes = []
+# for i in range(2, int(B**0.5)+1):
+#     if arr[i]:
+#         for j in range(i+i, B+1, i):
+#             arr[j] = False
+# for i in range(len(arr)):
+#     if arr[i]:
+#         primes.append(i)
+# print(primes)
