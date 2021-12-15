ffa00fdde6bf4b899fcbc9065b5678a3fb1a35a2
yyysolhhh <solbaram37@gmail.com>
2021-12-16 03:04:36 +0900

    211216

--- 백준 1456.py
+++ 백준 1456.py
@@ -6,7 +6,6 @@
 #     print(n)
 #     return True
 
-
 # A, B = map(int, input().split())
 # almst_prime = []
 # cnt = 0
@@ -22,39 +21,75 @@
 #         cnt += 1
 # print(cnt)
 
-# 2 - 1 메모리 초과
-A, B = map(int, input().split())
-arr = [False] * 2 + [True for _ in range(2, B+1)]
-primes = []
-almst_prime = []
-for i in range(2, B+1):
-    if arr[i]:
-        primes.append(i)
-        for j in range(i+i, B+1, i):
-            arr[j] = False
-for i in primes:
-    for j in range(2, B):
-        if i ** j > B:
-            break
-        almst_prime.append(i ** j)
-print(len(set(almst_prime)))
+# 2 - 1 메모리 초과, 틀림
+# A, B = map(int, input().split())
+# arr = [False] * 2 + [True for _ in range(2, B+1)]
+# primes = []
+# almst_prime = []
+# for i in range(2, B+1):
+#     if arr[i]:
+#         primes.append(i)
+#         for j in range(i+i, B+1, i):
+#             arr[j] = False
+# for i in primes:
+#     for j in range(2, B):
+#         if i ** j > B:
+#             break
+#         almst_prime.append(i ** j)
+# print(len(almst_prime))
 
-# 2 - 1 - 2
-A, B = map(int, input().split())
-arr = [False] * 2 + [True for _ in range(2, B+1)]
-primes = []
-almst_prime = []
-for i in range(2, B+1):
-    if arr[i]:
-        primes.append(i)
-        for j in range(i+i, B+1, i):
-            arr[j] = False
-for i in primes:
-    for j in range(2, B):
-        if i ** j > B:
-            break
-        almst_prime.append(i ** j)
-print(len(set(almst_prime)))
+# 2 - 1 - 2 메모리 초과, 틀림
+# A, B = map(int, input().split())
+# arr = [False] * 2 + [True for _ in range(2, B+1)]
+# almst_prime = []
+# for i in range(2, B+1):
+#     if arr[i]:
+#         for k in range(2, B):
+#             if i ** k > B:
+#                 break
+#             almst_prime.append(i ** k)
+#         for j in range(i+i, B+1, i):
+#             arr[j] = False
+# print(len(almst_prime))
+
+# 2 - 1 - 3 메모리 초과, 틀림
+# A, B = map(int, input().split())
+# arr = [False] * 2 + [True for _ in range(2, B+1)]
+# primes = []
+# almst_prime = []
+# cnt = 0
+# for i in range(2, B+1):
+#     if arr[i]:
+#         primes.append(i)
+#         for j in range(i+i, B+1, i):
+#             arr[j] = False
+# for alm in primes:
+#     i = alm
+#     while True:
+#         alm *= i
+#         if alm >= B:
+#             break
+#         else:
+#             cnt += 1
+# print(cnt)
+
+# 2 - 1 - 4 틀림
+# A, B = map(int, input().split())
+# rB = int(B**0.5)
+# arr = [False] * 2 + [True for _ in range(2, rB+1)]
+# cnt = 0
+# for i in range(2, rB+1):
+#     if arr[i]:
+#         k = i
+#         while True:
+#             i *= k
+#             if i >= B:
+#                 break
+#             else:
+#                 cnt += 1
+#         for j in range(k+k, rB+1, k):
+#             arr[j] = False
+# print(cnt)
 
 # 2 - 2
 # A, B = map(int, input().split())
---
+++ 백준 1629.py
@@ -0,0 +1,17 @@
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+
+def DnC(a, b):
+    if b == 1:
+        return a % C
+    temp = DnC(a, b//2)
+    if b % 2 == 0:
+        return temp * temp % C
+    else:
+        return temp * temp * a % C
+
+
+A, B, C = map(int, input().split())
+print(DnC(A, B))
