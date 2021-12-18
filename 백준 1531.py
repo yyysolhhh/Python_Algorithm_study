db59a3a59cc0d4839b29e1631c43b32b7396ac15
yyysolhhh <solbaram37@gmail.com>
2021-12-18 22:06:30 +0900

    211218 투명

--- 백준 1531.py
+++ 백준 1531.py
@@ -1,6 +1,13 @@
 N, M = map(int, input().split())
+pic = [[0 for _ in range(100)] for _ in range(100)]
 res = 0
 for _ in range(N):
     lx, ly, rx, ry = map(int, input().split())
-    res += (rx - lx) * (ry - ly)
+    for i in range(lx-1, rx):
+        for j in range(ly-1, ry):
+            pic[j][i] += 1
+for i in range(100):
+    for j in range(100):
+        if pic[j][i] > M:
+            res += 1
 print(res)
