2c487786e31cb823fe6fc8a88a1764391e680cfa
yyysolhhh <solbaram37@gmail.com>
2021-12-20 01:50:32 +0900

    211220

--- 백준 2529.py
+++ 백준 2529.py
@@ -1,12 +1,12 @@
+def check_lr(n, m, ine):
+    if ine == '<':
+        return n < m
+    elif ine == '>':
+        return n > m
+
+
 k = int(input())
 signs = list(input().split())
-num = [False for _ in range(9)]
-min_arr = []
-max_arr = []
-for i in range(k):
-    if signs[i] == '<':
-        for i in range(len(num)):
-            if not num[i]:
-                min_arr.append(i)
-                num[i] = True
-    elif signs[i] == '>':
+visited = [0 for i in range(9)]
+max_ans = ''
+min_ans = ''
