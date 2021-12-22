12689cb914eddf07e692ac9fc7080f25083c7953
yyysolhhh <solbaram37@gmail.com>
2021-12-23 00:37:07 +0900

    211223 수 이어쓰기

--- 백준 1515.py
+++ 백준 1515.py
@@ -1,28 +1,10 @@
-# def check(first, i):
-#     for j in range(len(first)):
-#         if i == first[j]:
-#             return first[j+1:]
-
-
-# num = input()
-# first = ''
-# n = 0
-# while len(first) < 3000:
-#     n += 1
-#     first += str(n)
-
-# for i in num:
-#     print(check(first, i))
-
-# 2
 remaining = list(input())
 i = 1
-while remaining:
-    print(i)
+while True:
     for j in str(i):
-        if remaining[0] == j:
+        if remaining and remaining[0] == j:
             remaining.pop(0)
-            break
-        i += 1
-    print(remaining)
+    if not remaining:
+        break
+    i += 1
 print(i)
