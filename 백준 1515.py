ca76eb3fb99cf530ec5b61a53ac35238903d7e1d
yyysolhhh <solbaram37@gmail.com>
2021-12-22 22:13:08 +0900

    211222 틀림

--- 백준 1515.py
+++ 백준 1515.py
@@ -1,15 +1,28 @@
-def check(first, i):
-    for j in range(len(first)):
-        if i == first[j]:
-            return first[j+1:]
+# def check(first, i):
+#     for j in range(len(first)):
+#         if i == first[j]:
+#             return first[j+1:]
 
 
-num = input()
-first = ''
-n = 0
-while len(first) < 3000:
-    n += 1
-    first += str(n)
+# num = input()
+# first = ''
+# n = 0
+# while len(first) < 3000:
+#     n += 1
+#     first += str(n)
 
-for i in num:
-    print(check(first, i))
+# for i in num:
+#     print(check(first, i))
+
+# 2
+remaining = list(input())
+i = 1
+while remaining:
+    print(i)
+    for j in str(i):
+        if remaining[0] == j:
+            remaining.pop(0)
+            break
+        i += 1
+    print(remaining)
+print(i)
