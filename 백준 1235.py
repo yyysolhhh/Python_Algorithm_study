c44c86a0179da84d7acbd6d27b858b31bfd2d989
yyysolhhh <solbaram37@gmail.com>
2021-12-25 02:06:59 +0900

    211225 학생 번호

--- 백준 1235.py
+++ 백준 1235.py
@@ -6,9 +6,7 @@ for i in range(1, 100):
     std_temp = []
     for j in range(N):
         std_temp.append(students[j][-i:])
-    # print(std_temp)
     if len(set(std_temp)) < N:
-        # print(set(std_temp))
         continue
     else:
         print(i)
