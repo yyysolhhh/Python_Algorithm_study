a448b71e5ce4f4126600449b67391d7601c9418e
yyysolhhh <solbaram37@gmail.com>
2021-12-30 01:16:05 +0900

    211230

--- 백준 10971.py
+++ 백준 10971.py
@@ -1,12 +1,13 @@
+from itertools import permutations
 import sys
 input = sys.stdin.readline
-# # 1
-# from itertools import permutations
+
+# 1 틀림
 # N = int(input())
 # W = []
 # for _ in range(N):
 #     W.append(list(map(int, input().split())))
-# paths = list(permutations(range(N), N))
+# paths = list(permutations(range(N)))
 # cost = 100000000
 # for p in paths:
 #     temp = 0
@@ -19,8 +20,28 @@ input = sys.stdin.readline
 # print(cost)
 
 # 2 비트마스크
+# N = int(input())
+# W = []
+# cost = 0
+# for _ in range(N):
+#     W.append(list(map(int, input().split())))
+
+# 1-2
 N = int(input())
 W = []
-cost = 0
 for _ in range(N):
     W.append(list(map(int, input().split())))
+paths = list(permutations(range(N)))
+cost = 100000000
+for p in paths:
+    if W[p[-1]][p[0]] != 0:
+        temp = 0
+        for i in range(N-1):
+            if W[p[i]][p[i+1]] == 0:
+                break
+            temp += W[p[i]][p[i+1]]
+            if temp >= cost:
+                break
+        temp += W[p[-1]][p[0]]
+        cost = min(cost, temp)
+print(cost)
