7f4fdb8e645ca0cddc783e554d7a2724b0b731f3
yyysolhhh <solbaram37@gmail.com>
2022-01-02 00:57:58 +0900

    220102 N과M

--- 백준 15649.py
+++ 백준 15649.py
@@ -1,3 +1,7 @@
+# 1 맞음
+from itertools import permutations
+
+
 def addNum():
     if len(combi) == M:
         print(' '.join(map(str, combi)))
@@ -13,3 +17,9 @@ def addNum():
 N, M = map(int, input().split())
 combi = []
 addNum()
+
+# 2 맞음
+N, M = map(int, input().split())
+perm = list(permutations(range(1, N+1), M))
+for i in perm:
+    print(*i)
