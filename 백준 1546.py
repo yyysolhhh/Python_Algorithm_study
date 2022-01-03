ee8ea5b835fbc3f0c94e2d931289ba9934351b7e
yyysolhhh <solbaram37@gmail.com>
2022-01-04 03:07:48 +0900

    220104 제출 버전

--- 백준 1546.py
+++ 백준 1546.py
@@ -3,7 +3,7 @@ N = int(input())
 score = list(map(int, sys.stdin.readline().split()))
 M = max(score)
 score = [i / M * 100 for i in score]
-# newSum = sum(score)
+newSum = 0
 for i in score:
     newSum += i
 print(newSum / N)
