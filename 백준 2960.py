485fb726ff775205023b010e07e2ba14878cf9f0
yyysolhhh <solbaram37@gmail.com>
2022-01-11 03:12:14 +0900

    220111 에라토스테네스의 체

--- 백준 2960.py
+++ 백준 2960.py
@@ -1,10 +1,12 @@
 N, K = map(int, input().split())
 prime = [False, False] + [True for _ in range(2, N+1)]
 for i in range(2, N+1):
-	if prime[i]:
-		for j in range(i+i, N+1, i):
-			prime[j] = False
-			K -= 1
-			if K == 0:
-				print(j)
-				break
+    if prime[i]:
+        for j in range(i, N+1, i):
+            if prime[j]:
+                prime[j] = False
+                K -= 1
+                # print(j, K)
+                if K == 0:
+                    print(j)
+                    break
