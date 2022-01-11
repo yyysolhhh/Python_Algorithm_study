5c6044707b24a3910c944d7d573e90c7f3ce6a43
yyysolhhh <solbaram37@gmail.com>
2022-01-12 01:59:23 +0900

    220112 수들의합2 두 포인터

--- 백준 2003.py
+++ 백준 2003.py
@@ -1,8 +1,24 @@
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 res = 0
-for i in range(N):
-    for j in range(i+1, N):
-        if sum(A[i:j+1]) == M:
-            res += 1
+
+# 1 시간 초과
+# for i in range(N):
+#     for j in range(i+1, N):
+#         if sum(A[i:j+1]) == M:
+#             res += 1
+# print(res)
+
+# 2
+start = 0
+end = 1
+while end <= N:
+    total = sum(A[start:end])
+    if total == M:
+        res += 1
+        end += 1
+    elif total < M:
+        end += 1
+    else:
+        start += 1
 print(res)
