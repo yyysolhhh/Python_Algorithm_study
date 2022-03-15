ab04cd3140e56c65c7136ef6a99c7245471e9dac
yyysolhhh <solbaram37@gmail.com>
2022-03-15 14:42:47 +0900

    220315 도비의 난독증 테스트

--- 백준 2204.py
+++ 백준 2204.py
@@ -1,11 +1,9 @@
-while (True):
+while True:
     n = int(input())
     words = []
+    order = {}
     if n == 0:
         break
     for _ in range(n):
         words.append(input())
-        for i in range(len(words)):
-            if 'A' <= words[i] or words[i] <= 'Z':
-                words[i] += 32
-    print(sorted(words)[0])
+    print(sorted(words, key=str.lower)[0])
