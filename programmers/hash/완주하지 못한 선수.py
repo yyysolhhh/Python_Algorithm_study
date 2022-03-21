750bfdd617abd67522e5fe2bb59d377899c488fc
yyysolhhh <solbaram37@gmail.com>
2022-03-21 14:56:45 +0900

    220321

--- programmers/hash/완주하지 못한 선수.py
+++ programmers/hash/완주하지 못한 선수.py
@@ -1,22 +1,4 @@
 def solution(participant, completion):
-    answer = ''
-
-    # for i in participant:
-    #     if i not in completion:
-    #         answer += i
-    #     else:
-    #         completion.remove(i)
-
-    # par_list = {}
-    # for i in participant:
-    #     par_list[i] = 0
-    # for i in completion:
-    #     par_list[i] = 1
-    # for i, j in par_list.items():
-    #     if j == 0:
-    #         answer += i
-    # return answer
-
     participant.sort()
     completion.sort()
     for i in range(len(completion)):
