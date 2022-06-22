66845fa2dfb8567438e0b776952e43b5c225e7de
yyysolhhh <solbaram37@gmail.com>
2022-06-23 03:33:26 +0900

    220623 solving lv2

---
+++ programmers/sorting/lv2_가장 큰 수.py
@@ -0,0 +1,17 @@
+# 시간 초과
+# from itertools import permutations
+# def solution_timeout(numbers):
+#     numbers = list(map(str, numbers))
+#     perm = list(map(''.join, permutations(numbers)))
+#     return max(perm)
+
+
+def solution(numbers):
+
+    return
+
+
+print(solution([6, 10, 2]))
+print(solution([3, 30, 34, 5, 9]))
+print(solution_timeout([6, 10, 2]))
+print(solution_timeout([3, 30, 34, 5, 9]))
