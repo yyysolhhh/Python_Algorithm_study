113f25ae8225f03760777f08d94a26b8ec83a91e
yyysolhhh <solbaram37@gmail.com>
2022-06-25 15:35:12 +0900

    220625 / bj1697 숨바꼭질 / bfs

---
+++ baekjoon/백준 1697.py
@@ -0,0 +1,27 @@
+from collections import deque
+import sys
+
+
+def bfs(N):
+    queue = deque([N])
+    while (queue):
+        # 1. 큐에서 꺼내기
+        curr = queue.popleft()
+        # 2. 목적지인가?
+        if curr == K:
+            return (visited[curr])
+        # 3. 연결된 곳 순회
+        for i in (curr - 1, curr + 1, curr * 2):
+            # 4. 갈 수 있는가?
+            if 0 <= i <= 100000 and not visited[i]:
+                # 5. 체크인
+                visited[i] = visited[curr] + 1
+                # 6. 큐에 넣음
+                queue.append(i)
+
+
+input = sys.stdin.readline
+N, K = map(int, input().split())
+visited = [0 for _ in range(100001)]
+
+print(bfs(N))
