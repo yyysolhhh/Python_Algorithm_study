e54ddfb0ea330aa9c5e21be57107dfc986fc23b7
yyysolhhh <solbaram37@gmail.com>
2022-07-05 23:54:49 +0900

    220705 / bj11478 서로 다른 부분 문자열의 개수 / set

--- baekjoon/백준 11478.py
+++ baekjoon/백준 11478.py
@@ -1,8 +1,6 @@
 S = input()
 strs = []
-for i in range(len(S) - 1):
+for i in range(len(S)):
     for j in range(i + 1, len(S) + 1):
         strs.append(S[i:j])
-print(strs)
-print(set(strs))
 print(len(set(strs)))
