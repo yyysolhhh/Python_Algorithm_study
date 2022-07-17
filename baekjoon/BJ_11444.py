46389b7a0d0e35368a6754236afbd88b228aadcc
yyysolhhh <solbaram37@gmail.com>
2022-07-17 16:04:55 +0900

    220717 rename 백준 ->BJ_

---
+++ baekjoon/BJ_11444.py
@@ -0,0 +1,13 @@
+# def fibonacci(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     return (fibonacci(n - 2) + fibonacci(n - 1)) % 1000000007
+
+
+# n = int(input())
+# print(fibonacci(n))
+
+n = int(input())
+f_matrix = [[1, 1], [1, 0]]
---
+++ baekjoon/BJ_11725.py
@@ -0,0 +1,3 @@
+N = int(input())
+for _ in range(N):
+    p1, p2 = map(int, input().split())
