c37def853edfc1dfe1f28a6aaf0ab65043d2c383
yyysolhhh <solbaram37@gmail.com>
2022-07-18 16:39:49 +0900

    220718 / bj11444 피보나치 수 6 / unsolved

--- baekjoon/BJ_11444.py
+++ baekjoon/BJ_11444.py
@@ -10,5 +10,9 @@
 # print(fibonacci(n))
 
 n = int(input())
-f_matrix = [[1, 1], [1, 0]]
-print()
+f = [[1, 1], [1, 0]]
+temp = [[1, 1], [1, 0]]
+for _ in range(n - 1):
+    temp = [[temp[0][0] * f[0][0] + temp[0][1] * f[1][0], temp[0][0] * f[0][1] + temp[0][1] * f[1][1]],
+            [temp[1][0] * f[0][0] + temp[1][1] * f[1][0], temp[1][0] * f[0][1] + temp[1][1] * f[1][1]]]
+print(temp[0][1] % 1000000007)
