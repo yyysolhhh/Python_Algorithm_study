49273c109f2b86b242bd330f64c7ca41a4763711
yyysolhhh <solbaram37@gmail.com>
2022-07-21 12:52:05 +0900

    220721 / bj1388 바닥 장식 / graph

--- baekjoon/BJ_1388.py
+++ baekjoon/BJ_1388.py
@@ -1,13 +1,24 @@
+import sys
+
+input = sys.stdin.readline
 N, M = map(int, input().split())
 floor = []
 ans = 0
 for _ in range(N):
-    floor.append(input())
-for i in range(len(floor)):
-    for j in range(len(i)):
-        if floor[i][j] == '-':
-            if floor[i][j-1] == '-':
-                ans += 0
-            else:
-                ans += 1
-        elif floor[i][j] == '|':
+    floor.append(list(input()))
+
+for y in range(N):
+    for x in range(M):
+        if floor[y][x] == '-':
+            ny, nx = y, x
+            while nx < M and floor[ny][nx] == '-':
+                floor[ny][nx] = 1
+                nx += 1
+            ans += 1
+        elif floor[y][x] == '|':
+            ny, nx = y, x
+            while ny < N and floor[ny][nx] == '|':
+                floor[ny][nx] = 1
+                ny += 1
+            ans += 1
+print(ans)
