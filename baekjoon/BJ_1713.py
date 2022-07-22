176ecf6b6ae4c905c6b36127edd7c1f734a76874
yyysolhhh <solbaram37@gmail.com>
2022-07-22 15:37:21 +0900

    220722 / bj1713 후보 추천하기 / unsolved

---
+++ baekjoon/BJ_1713.py
@@ -0,0 +1,27 @@
+N = int(input())
+total = int(input())
+student = list(map(int, input().split()))
+recommended = dict.fromkeys(student, 0)
+cnt = 0
+frame = []
+
+for i in student:
+    print(frame, recommended, cnt)
+    if i not in frame:
+        frame.append(i)
+        recommended[i] += 1
+        cnt += 1
+        if cnt > N:
+            min_rec = len(student)
+            min_stu = frame[0]
+            for j in frame:
+                if recommended[j] < min_rec:
+                    min_rec = recommended[j]
+                    min_stu = j
+            frame.remove(min_stu)
+            recommended[min_stu] = 0
+            cnt -= 1
+    else:
+        recommended[i] += 1
+
+print(' '.join(sorted(map(str, frame))))
