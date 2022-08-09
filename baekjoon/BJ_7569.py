a91cc6685f6ad1013263732f17a728b00589ad52
yyysolhhh <solbaram37@gmail.com>
2022-08-10 06:55:45 +0900

    220810 / bj7569 토마토 / bfs

--- baekjoon/BJ_7569.py
+++ baekjoon/BJ_7569.py
@@ -7,30 +7,33 @@ dx, dy, dh = [-1, 1, 0, 0, 0, 0], [0, 0, -1, 1, 0, 0], [0, 0, 0, 0, -1, 1]
 def bfs():
     while queue:
         h, y, x = queue.popleft()
-        print(h, y, x)
-        if h == H - 1 and y == Y - 1 and x == X - 1:
-            return box[h][y][x]
         for i in range(6):
+            nh = h + dh[i]
             ny = y + dy[i]
             nx = x + dx[i]
-            nh = h + dh[i]
-            if 0 <= ny < N and 0 <= nx < M and 0 <= nh < H and box[nh][ny][nx] == 0 and box[nh][ny][nx] != -1:
+            if 0 <= nh < H and 0 <= ny < N and 0 <= nx < M and box[nh][ny][nx] == 0:
                 box[nh][ny][nx] = box[h][y][x] + 1
-                queue.append((h, y, x))
-    return (-1)
+                queue.append((nh, ny, nx))
 
 
 input = sys.stdin.readline
 M, N, H = map(int, input().split())
 box = [[] for _ in range(H)]
-for i in range(H):
-    for _ in range(N):
-        box[i].append(list(map(int, input().split())))
-print(box)
 queue = deque()
-for h, H in enumerate(box):
-    for y, Y in enumerate(H):
-        for x, X in enumerate(Y):
-            if X == 1:
+res = 0
+for h in range(H):
+    for y in range(N):
+        box[h].append(list(map(int, input().split())))
+        for x in range(M):
+            if box[h][y][x] == 1:
                 queue.append((h, y, x))
-print(bfs())
+bfs()
+for h in box:
+    for y in h:
+        for x in y:
+            if x == 0:
+                print(-1)
+                exit()
+            else:
+                res = max(res, x)
+print(res - 1)
