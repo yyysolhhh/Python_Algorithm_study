ab8a7612e81edd067cf807b0ce100b35d7a529e7
yyysolhhh <solbaram37@gmail.com>
2024-01-04 19:01:31 +0900

    ps programmers lv1 zip 활용

--- programmers/Level1/lv1_행렬의 덧셈.py
+++ programmers/Level1/lv1_행렬의 덧셈.py
@@ -4,3 +4,7 @@ def solution(arr1, arr2):
         for j in range(len(arr1[i])):
             result[i][j] = arr1[i][j] + arr2[i][j]
     return result
+
+def solution2(arr1, arr2):
+    result = [[c + d for c, d in zip(a, b)] for a, b in zip(arr1, arr2)]
+    return result
