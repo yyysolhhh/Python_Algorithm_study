5384454ba79c476ceda6d6b46f0928f4810bbdbf
yyysolhhh <solbaram37@gmail.com>
2024-01-07 14:39:33 +0900

    add BJ_1439

--- baekjoon/BJ_1439.py
+++ baekjoon/BJ_1439.py
@@ -1,9 +1,17 @@
+#s = input()
+#res = 0
+#for i, c in enumerate(s[1:]):
+#    if c != s[i]:
+#        res += 1
+#if res % 2 == 0:
+#    print(res // 2)
+#else:
+#    print(res // 2 + 1)
+
+# 다른 풀이
 s = input()
-res = 0
+res = 1
 for i, c in enumerate(s[1:]):
     if c != s[i]:
         res += 1
-if res % 2 == 0:
-    print(res // 2)
-else:
-    print(res // 2 + 1)
+print(res // 2)
