1bb3f7ed7bf2faf54a9a2d60eca2e76c615386da
yyysolhhh <solbaram37@gmail.com>
2024-01-09 02:28:27 +0900

    Solve: BJ_1246
    그리디

---
+++ unsolved/BJ_1246.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+P = []
+for _ in range(M):
+    P.append(int(input()))
+prices = sorted(P, reverse = True)
+profit = 0
+for i, p in enumerate(prices):
+    i = min(N - 1, i)
+    temp = p * (i + 1)
+    if profit < temp:
+        profit = profit, temp
+        fixed_p = p
+#    if profit >= temp:
+#       break
+#    profit = max(profit, temp)
+#    fixed_p = p
+print(fixed_p, profit)
