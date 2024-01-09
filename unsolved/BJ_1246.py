c4dac0fb758db5ab42e3fcab5d37fa11726e2723
yyysolhhh <solbaram37@gmail.com>
2024-01-09 13:04:37 +0900

    Refactor: enumerate에 start 인자 넣음

--- unsolved/BJ_1246.py
+++ unsolved/BJ_1246.py
@@ -7,14 +7,25 @@ for _ in range(M):
     P.append(int(input()))
 prices = sorted(P, reverse = True)
 profit = 0
-for i, p in enumerate(prices):
-    i = min(N - 1, i)
-    temp = p * (i + 1)
+for i, p in enumerate(prices, start=1):
+    i = min(N, i)
+    temp = p * i
     if profit < temp:
-        profit = profit, temp
+        profit = temp
         fixed_p = p
+#for i, p in enumerate(prices):
+#    i = min(N - 1, i)
+#    temp = p * (i + 1)
+#    if profit < temp:
+#        profit = temp
+#        fixed_p = p
+#    else:
+#        break
+
 #    if profit >= temp:
 #       break
 #    profit = max(profit, temp)
 #    fixed_p = p
 print(fixed_p, profit)
+
+# 왜 중간에 멈추면 안되는지 알아내기
