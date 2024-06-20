058a3d0f9b50718d5b984bcfb3028ae7e0bb3273
yyysolhhh <solbaram37@gmail.com>
2024-06-20 16:08:10 +0900

    Unsolved: unsolved 문제 추가

--- unsolved/BJ_11444.py
+++ unsolved/BJ_11444.py
@@ -1,3 +1,4 @@
+# 런타임에러
 # def fibonacci(n):
 #     if n == 0:
 #         return 0
@@ -9,10 +10,14 @@
 # n = int(input())
 # print(fibonacci(n))
 
+# 시간초과
+# n = int(input())
+# f = [[1, 1], [1, 0]]
+# temp = [[1, 1], [1, 0]]
+# for _ in range(n - 1):
+#     temp = [[temp[0][0] * f[0][0] + temp[0][1] * f[1][0], temp[0][0] * f[0][1] + temp[0][1] * f[1][1]],
+#             [temp[1][0] * f[0][0] + temp[1][1] * f[1][0], temp[1][0] * f[0][1] + temp[1][1] * f[1][1]]]
+# print(temp[0][1] % 1000000007)
+
 n = int(input())
 f = [[1, 1], [1, 0]]
-temp = [[1, 1], [1, 0]]
-for _ in range(n - 1):
-    temp = [[temp[0][0] * f[0][0] + temp[0][1] * f[1][0], temp[0][0] * f[0][1] + temp[0][1] * f[1][1]],
-            [temp[1][0] * f[0][0] + temp[1][1] * f[1][0], temp[1][0] * f[0][1] + temp[1][1] * f[1][1]]]
-print(temp[0][1] % 1000000007)
---
+++ unsolved/BJ_2729.py
@@ -0,0 +1,4 @@
+T = int(input())
+for _ in range(T):
+    num1, num2 = map(str, input().split())
+    print(num1 + num2)
