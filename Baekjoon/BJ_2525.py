81e7da8b6bc783b0e82621905315bf6919582613
yyysolhhh <solbaram37@gmail.com>
2024-07-16 23:53:09 +0900

    Solve: BJ_2525

--- Baekjoon/BJ_2525.py
+++ Baekjoon/BJ_2525.py
@@ -1,9 +1,19 @@
+# 1
+#A, B = map(int, input().split())
+#C = int(input())
+#B += C
+#if B >= 60:
+#    A += B // 60
+#    B %= 60
+#if A >= 24:
+#    A %= 24
+#print(A, B)
+
+# 2
 A, B = map(int, input().split())
 C = int(input())
-B += C
-if B >= 60:
-    A += B // 60
-    B %= 60
-if A >= 24:
-    A %= 24
-print(A, B)
+total_min = A * 60 + B + C
+h = (total_min // 60) % 24
+m = total_min % 60
+print(h, m)
+
