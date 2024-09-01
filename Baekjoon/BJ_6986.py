0bfa9892b2999dc31b0e78200927df9278a2a0f5
yyysolhhh <solbaram37@gmail.com>
2024-09-02 00:02:56 +0900

    Solve: BJ_6986

---
+++ Baekjoon/BJ_6986.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+scores = []
+for _ in range(N):
+    scores.append(float(input()))
+scores.sort()
+adjusted = scores[K] * K + scores[N - K - 1] * K
+trimmed = 0
+for i in range(K, N - K):
+    adjusted += scores[i]
+    trimmed += scores[i]
+trimmed /= N - 2 * K
+adjusted /= N
+print("{:.2f}".format(trimmed + 1e-8))
+print("{:.2f}".format(adjusted + 1e-8))
+print(f"{trimmed + 1e-8:.2f}")
+print(f"{adjusted + 1e-8:.2f}")
+print("%.2f" %(trimmed + 1e-8))
+print("%.2f" %(adjusted + 1e-8))
