619746f859b8394f443843027573071349d2317d
yyysolhhh <solbaram37@gmail.com>
2025-01-06 23:39:19 +0900

    Solve: BJ_23253

---
+++ Baekjoon/BJ_23253.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+ans = "Yes"
+for _ in range(M):
+    k = int(input())
+    dummy = list(map(int, input().split()))
+    for i in range(k - 1):
+        if dummy[i + 1] - dummy[i] > 0:
+            ans = "No"
+            break
+    if ans == "No":
+        break
+print(ans)
