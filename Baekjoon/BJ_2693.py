5f14318b609210833c54e29cdc93976d85479462
yyysolhhh <solbaram37@gmail.com>
2025-08-23 23:44:16 +0900

    Solve: BJ_2693

--- Baekjoon/BJ_2693.py
+++ Baekjoon/BJ_2693.py
@@ -1,12 +1,4 @@
 T = int(input())
-A = [[0 for i in range(10)]for i in range(T)]
-for i in range(T):
-    A[i] = list(map(int, input().split()))
-N = 3
-print(A)
-for i in range(T):
-    for j in range(len(A)):
-        if A[i][j] < A[i][j+1]:
-            A[i][j], A[i][j+1] = A[i][j+1], A[i][j]
-for i in range(T):
-    print(A[i][2])
+for _ in range(T):
+    A = list(map(int, input().split()))
+    print(sorted(A)[-3])
