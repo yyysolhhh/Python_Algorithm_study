2428cba6761f3a44355263f8c80b74bdb579382d
yyysolhhh <solbaram37@gmail.com>
2025-11-03 17:53:55 +0900

    Solve: BJ_7453

---
+++ Baekjoon/BJ_7453.py
@@ -0,0 +1,44 @@
+import sys
+from collections import Counter
+input = sys.stdin.readline
+n = int(input())
+A, B, C, D = [], [], [], []
+for i in range(n):
+    a, b, c, d = map(int, input().split())
+    A.append(a)
+    B.append(b)
+    C.append(c)
+    D.append(d)
+
+# 1
+ans = 0
+AB = [a + b for a in A for b in B]
+CD = Counter([c + d for c in C for d in D])
+for i in AB:
+    ans += CD[-i]
+print(ans)
+
+# 2
+ans = 0
+AB = sorted(a + b for a in A for b in B)
+CD = sorted(c + d for c in C for d in D)
+ab, cd = 0, len(CD) - 1
+while ab < len(AB) and cd >= 0:
+    tot = AB[ab] + CD[cd]
+    if tot == 0:
+        cur_ab = ab
+        cur_cd = cd
+        ab_cnt = 0
+        while ab < len(AB) and AB[ab] == AB[cur_ab]:
+            ab += 1
+            ab_cnt += 1
+        cd_cnt = 0
+        while cd >= 0 and CD[cd] == CD[cur_cd]:
+            cd -= 1
+            cd_cnt += 1
+        ans += ab_cnt * cd_cnt
+    elif tot < 0:
+        ab += 1
+    else:
+        cd -= 1
+print(ans)
