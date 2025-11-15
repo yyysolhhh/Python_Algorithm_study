d098b6b21646a65d31ce3da6f9eb514097388614
yyysolhhh <solbaram37@gmail.com>
2025-11-15 23:39:39 +0900

    Solve: BJ_5692

---
+++ Baekjoon/BJ_5692.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+mul = [1 for _ in range(10)]
+for i in range(1, 10):
+    mul[i] = i * mul[i - 1]
+print(mul)
+while True:
+    n = input().strip()
+    if n == "0":
+        break
+    ans = 0
+    for i, j in enumerate(n[::-1]):
+        ans += int(j) * mul[int(i + 1)]
+    print(ans)
