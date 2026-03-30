c1c3c9dc2531478eed33af04f2105b973513c4eb
yyysolhhh <solbaram37@gmail.com>
2026-03-30 23:33:05 +0900

    Solve: BJ_24724

---
+++ Baekjoon/BJ_24724.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+for i in range(1, T + 1):
+    N = int(input())
+    A, B = map(int, input().split())
+    for _ in range(N):
+        u, v = map(int, input().split())
+    print(f"Material Management {i}")
+    print("Classification ---- End!")
